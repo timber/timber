cd85801ae1b4c69cc286258d4ca8a36e07f83853
Jared Novack <jared@upstatement.com>
2013-05-07 22:49:09 -0400

    adding new utile to WPHelper for trimming words

--- functions/functions-wp-helper.php
+++ functions/functions-wp-helper.php
@@ -2,10 +2,6 @@
 
 	class WPHelper {
 
-		function __construct(){
-			add_filter('cron_schedules', array($this, 'cron_add_quarterly'));
-		}
-
 		function is_url($url){
 			$url = strtolower($url);
 			if (strstr('http', $url)){
@@ -23,6 +19,83 @@
 			$wpdb->query("UPDATE $wpdb->options SET option_value = '$value' WHERE option_name = '$option'");
 		}
 
+		function trim_words( $text, $num_words = 55, $more = null, $allowed_tags = 'p a span b i br' ) {
+			if ( null === $more )
+				$more = __( '&hellip;' );
+			$original_text = $text;
+			$allowed_tag_string = '';
+			foreach(explode(' ', $allowed_tags) as $tag){
+				$allowed_tag_string .= '<'.$tag.'>';
+			}
+			$text = strip_tags($text, $allowed_tag_string);
+			/* translators: If your word count is based on single characters (East Asian characters),
+			   enter 'characters'. Otherwise, enter 'words'. Do not translate into your own language. */
+			if ( 'characters' == _x( 'words', 'word count: words or characters?' ) && preg_match( '/^utf\-?8$/i', get_option( 'blog_charset' ) ) ) {
+				$text = trim( preg_replace( "/[\n\r\t ]+/", ' ', $text ), ' ' );
+				preg_match_all( '/./u', $text, $words_array );
+				$words_array = array_slice( $words_array[0], 0, $num_words + 1 );
+				$sep = '';
+			} else {
+				$words_array = preg_split( "/[\n\r\t ]+/", $text, $num_words + 1, PREG_SPLIT_NO_EMPTY );
+				$sep = ' ';
+			}
+			if ( count( $words_array ) > $num_words ) {
+				array_pop( $words_array );
+				$text = implode( $sep, $words_array );
+				$text = $text . $more;
+			} else {
+				$text = implode( $sep, $words_array );
+			}
+			$text = self::close_tags($text);
+			return apply_filters( 'wp_trim_words', $text, $num_words, $more, $original_text );
+		}
+
+		function trim_text($input, $length, $strip_html = true, $ellipses = '') {
+		    //strip tags, if desired
+		    if ($strip_html) {
+		        $input = strip_tags($input);
+		    }
+		  
+		    //no need to trim, already shorter than trim length
+		    if (strlen($input) <= $length) {
+		        return $input;
+		    }
+		  
+		    //find last space within length
+		    $last_space = strrpos(substr($input, 0, $length), ' ');
+		    $trimmed_text = substr($input, 0, $last_space);
+		  
+		    //add ellipses (...)
+		    if ($ellipses) {
+		        $trimmed_text .= $ellipses;
+		    }
+		    return $trimmed_text;
+		}
+
+		function close_tags($html) {
+  			#put all opened tags into an array
+  			preg_match_all('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
+  			$openedtags = $result[1];   
+  			#put all closed tags into an array
+			preg_match_all('#</([a-z]+)>#iU', $html, $result);
+			$closedtags = $result[1];
+			$len_opened = count($openedtags);
+			# all tags are closed
+			if (count($closedtags) == $len_opened) {
+				return $html;
+			}
+			$openedtags = array_reverse($openedtags);
+			# close tags
+			for ($i=0; $i < $len_opened; $i++) {
+				if (!in_array($openedtags[$i], $closedtags)){
+					$html .= '</'.$openedtags[$i].'>';
+				} else {
+			  		unset($closedtags[array_search($openedtags[$i], $closedtags)]);   
+			  	}
+			}  
+			return $html;
+		} 
+
 		/* this $args thing is a fucking mess, fix at some point: 
 
 		http://codex.wordpress.org/Function_Reference/comment_form */
@@ -36,5 +109,3 @@
 		}
 
 	}
-
-	new WPHelper();
