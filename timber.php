090d4fbbcfc9e42ed1b24958be2d8ff732e6e36a
Jared Novack <jared@upstatement.com>
2013-05-14 20:59:16 -0400

    killing some error logging

--- timber.php
+++ timber.php
@@ -59,19 +59,19 @@ class Timber {
 
 	public function get_posts($query = false, $PostClass = 'TimberPost'){
 		if (PHPHelper::is_array_assoc($query) || is_string($query)){
-			//we have a reguarlly formed WP query string or array to use
+			// we have a reguarlly formed WP query string or array to use
+			WPHelper::error_log('GOOD QUERY');
+			WPHelper::error_log($query);
+			WPHelper::error_log('----------');
 			return self::get_posts_from_wp_query($query, $PostClass);
 		} else if (is_array($query) && count($query) && (is_integer($query[0]) || is_string($query[0]))){
-			//we have a list of pids to extract from
+			// we have a list of pids to extract from
 			return self::get_posts_from_array_of_ids($query, $PostClass);
 		} else if(is_array($query) && count($query) && isset($query[0]) && is_object($query[0])){
-			error_log("USE ME!!!");
+			// hmmm -- maybe its an array of post objects already
 			return self::handle_post_results($query, $PostClass);
 		} else if (have_posts()){
-			error_log('using the post query');
-			error_log('am I an array? '.is_array($query));
-			error_log('but do i have a count of ='.count($query));
-			error_log('is the first one of me an object '.is_object($query[0]));
+			//lets just use the default WordPress current query
 			WPHelper::error_log(debug_backtrace());
 			return self::get_posts_from_loop($PostClass);
 		} else if (!$query){
