98f247dd60b6da03216eb909f17a1bcd1598f7b9
Jared Novack <jared@upstatement.com>
2013-06-14 14:50:42 -0400

    completed basic menu implementation

--- objects/timber-menu.php
+++ objects/timber-menu.php
@@ -2,9 +2,23 @@
 	
 	class TimberMenu {
 
+		var $items = null;
+
 		function __construct($slug){
-			$menu = wp_get_nav_menu_object($slug);
-			$menu = wp_get_nav_menu_items($menu);
-			return $menu;
+			//$menu = wp_get_nav_menu_object($slug);
+			$locations = get_nav_menu_locations();
+			if (isset($locations[$slug])){
+				$menu = wp_get_nav_menu_object($locations[$slug]);
+				$menu = wp_get_nav_menu_items($menu);
+				$this->items = $menu;
+			}
+			return null;
+		}
+
+		function get_items(){
+			if (is_array($this->items)){
+				return $this->items;
+			}
+			return array();
 		}
 	}
