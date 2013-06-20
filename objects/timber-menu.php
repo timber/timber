3855eb612613ab467d21660cc7416675e95b3e14
Jared Novack <jared@Jareds-MacBook-Air.local>
2013-06-19 20:00:36 -0400

    timber-menu is now everything you ever wanted

--- objects/timber-menu.php
+++ objects/timber-menu.php
@@ -10,9 +10,35 @@
 			if (isset($locations[$slug])){
 				$menu = wp_get_nav_menu_object($locations[$slug]);
 				$menu = wp_get_nav_menu_items($menu);
+				$menu = self::order_children($menu);
 				$this->items = $menu;
 			}
 			return null;
+		}
+
+		function find_parent_item_in_menu($menu_items, $parent_id){
+			foreach($menu_items as &$item){
+				if ($item->ID == $parent_id){
+					return $item;
+				}
+			}
+			return null;
+		}
+
+		function order_children($items){
+			$menu = array();
+			foreach($items as $item){
+				if($item->menu_item_parent == 0){
+					$menu[] = new TimberMenuItem($item);
+					continue;
+				}
+				$parent_id = $item->menu_item_parent;
+				$parent = self::find_parent_item_in_menu($menu, $parent_id);
+				if ($parent){
+					$parent->add_child(new TimberMenuItem($item));
+				}
+			}
+			return $menu;
 		}
 
 		function get_items(){
@@ -20,5 +46,32 @@
 				return $this->items;
 			}
 			return array();
+		}
+	}
+
+	class TimberMenuItem extends TimberCore {
+
+		var $children;
+
+		function __construct($data){
+			$this->import($data);
+		}
+
+		function get_path(){
+			return $this->url_to_path($this->url);
+		}
+
+		function add_child($item){
+			if (!isset($this->children)){
+				$this->children = array();
+			}
+			$this->children[] = $item;
+		}
+
+		function get_children(){
+			if (isset($this->children)){
+				return $this->children;
+			}
+			return false;
 		}
 	}
