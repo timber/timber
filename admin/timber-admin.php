acb11ddb8ff8112983f3c61874a749753d6bf3f6
Jared Novack <jared@Jareds-MacBook-Air.local>
2013-07-11 23:18:47 -0400

    update timber admin page

--- admin/timber-admin.php
+++ admin/timber-admin.php
@@ -16,7 +16,11 @@
 
 		function create_admin_page(){
 			$data = array();
+			$data['theme_dir'] = get_stylesheet_directory();
+			$data['home_file']['name'] = 'index.php';
 			$data['timber_base'] = TIMBER_URL_PATH;
+			$data['home_file']['path'] = trailingslashit(get_stylesheet_directory()).$data['home_file']['name'];
+			$data['home_file']['contents'] = htmlentities(file_get_contents(realpath($data['home_file']['path'])));
 			Timber::render('timber-admin.twig', $data);
 		}
 
