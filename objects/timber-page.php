9f392b320c79012901339b6329f880b8e6ccf23b
Jared Novack <jarednova@upstatement.com>
2013-07-25 14:48:06 -0400

    TimberPage had some gunk that needed removal

--- objects/timber-page.php
+++ objects/timber-page.php
@@ -1,19 +1,9 @@
 <?php
 
-	class TimberPage {
-		
-		function __construct($path, $file){
+class TimberPage extends TimberPost {
 
-		}
-		
+	function __construct($path, $file){
+
 	}
 
-	Timber::add_route('/articles/page/$p', function($page){
-		if ($page > 1){
-			$query['paged'] = $page;
-		}
-	});
-
-	Timber::add_route('/blog/', function($page){
-		
-	});
+}
