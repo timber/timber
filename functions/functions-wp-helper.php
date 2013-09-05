bad913587696e9f9f5200167eb3774c5bce11c02
Jared Novack <jarednova@upstatement.com>
2013-09-04 23:23:23 -0400

    fixed issue in pagination title for current page

--- functions/functions-wp-helper.php
+++ functions/functions-wp-helper.php
@@ -386,7 +386,7 @@ class WPHelper {
 			$n_display = number_format_i18n($n);
 			if ( $n == $current ) :
 				//$page_links[] = "<span class='page-numbers current'>$n_display</span>";
-				$page_links[] = array('class' => 'page-number current', 'text' => $n_display);
+				$page_links[] = array('class' => 'page-number current', 'title' => $n_display);
 				$dots = true;
 			else :
 				if ( $show_all || ( $n <= $end_size || ( $current && $n >= $current - $mid_size && $n <= $current + $mid_size ) || $n > $total - $end_size ) ) :
