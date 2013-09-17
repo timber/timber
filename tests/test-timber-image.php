eb1a24fbf23f8d96c81aaa836ad9f5a05bfebb0a
Jared Novack <jarednova@upstatement.com>
2013-09-17 17:58:12 -0400

    more image test coverage, this time for thumbnail attachments

--- tests/test-timber-image.php
+++ tests/test-timber-image.php
@@ -24,17 +24,26 @@ class TimberImageTest extends WP_UnitTestCase {
 		$exists = file_exists($resized_path);
 		$this->assertTrue($exists);
 		$old_time = filemtime($resized_path);
-		sleep(3);
+		sleep(2);
 		Timber::render('assets/image-test.twig', $data);
 		$new_time = filemtime($resized_path);
 		$this->assertEquals($old_time, $new_time);
+	}
+
+	function copyTestImage($img = 'arch.jpg'){
+		$upload_dir = wp_upload_dir();
+		$destination = $upload_dir['path'].'/'.$img;
+		if (!file_exists($destination)){
+			copy(__DIR__.'/assets/'.$img, $destination);
+		}
+		return $destination;
 	}
 
 	function testImageResize(){
 		$data = array();
 		$data['size'] = array('width' => 600, 'height' => 400);
 		$upload_dir = wp_upload_dir();
-		copy(__DIR__.'/assets/arch.jpg', $upload_dir['path'].'/arch.jpg');
+		$this->copyTestImage();
 		$url = $upload_dir['url'].'/arch.jpg';
 		$data['test_image'] = $url;
 		Timber::render('assets/image-test.twig', $data);
@@ -43,14 +52,41 @@ class TimberImageTest extends WP_UnitTestCase {
 		$this->assertTrue($exists);
 		//Now make sure it doesnt regenerage
 		$old_time = filemtime($resized_path);
-		sleep(3);
+		sleep(2);
 		Timber::render('assets/image-test.twig', $data);
 		$new_time = filemtime($resized_path);
 		error_log('time is '.$old_time);
 		$this->assertEquals($old_time, $new_time);
 	}
 
-	
+	function testInitFromURL(){
+		$destination_path = $this->copyTestImage();
+		$destination_url = str_replace(ABSPATH, 'http://'.$_SERVER['HTTP_HOST'].'/', $destination_path);
+		$image = new TimberImage($destination_url);
+		$this->assertEquals($destination_url, $image->get_src());
+		$this->assertEquals($destination_url, (string)$image);
+	}
+
+	function testPostThumbnails(){
+		$post_id = $this->factory->post->create();
+		$filename = $this->copyTestImage('flag.png');
+		$destination_url = str_replace(ABSPATH, 'http://'.$_SERVER['HTTP_HOST'].'/', $filename);
+		$wp_filetype = wp_check_filetype(basename($filename), null );
+		$attachment = array(
+			'post_mime_type' => $wp_filetype['type'],
+			'post_title' => preg_replace('/\.[^.]+$/', '', basename($filename)),
+			'post_content' => '',
+			'post_status' => 'inherit'
+		);
+		$attach_id = wp_insert_attachment( $attachment, $filename, $post_id );
+		add_post_meta($post_id, '_thumbnail_id', $attach_id, true);
+		$data = array();
+		$data['post'] = new TimberPost($post_id);
+		$data['size'] = array('width' => 100, 'height' => 50);
+		Timber::render('assets/thumb-test.twig', $data);
+		$exists = file_exists($filename);
+		$this->assertTrue($exists);
+	}
 
 
 }
