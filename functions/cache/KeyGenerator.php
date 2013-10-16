5ac08fdacfae295eed7463d19552a4ad594ef031
Mike <mike@trenvo.com>
2013-10-16 20:36:15 +0200

    Added TimberKeyGeneratorInterface for classes to generate their own cache key. If array is passed to cache and key '_cache_key' is available, that value is used.

--- functions/cache/KeyGenerator.php
+++ functions/cache/KeyGenerator.php
@@ -8,6 +8,12 @@ class KeyGenerator implements KeyGeneratorInterface
 {
 
     public function generateKey( $value ) {
+        if ( is_a( $value, 'TimberKeyGeneratorInterface' ) )
+            return $value->_get_cache_key();
+
+        if ( is_array( $value ) && isset( $value['_cache_key'] ) )
+            return $value['_cache_key'];
+
         $key = md5( json_encode( $value ) );
         if ( is_object( $value ) )
             $key = get_class( $key ) . '|' . $key;
