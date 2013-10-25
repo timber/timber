75ed994f75248b48c9f2e5bcca34e4e18df443fd
Jared Novack <jarednova@upstatement.com>
2013-10-25 10:08:54 -0400

    fixed prev/next which were reversed

--- functions/timber-post.php
+++ functions/timber-post.php
@@ -228,14 +228,14 @@ class TimberPost extends TimberCore {
 
 	function get_next() {
 		if (!isset($this->next)){
-			$this->next = new $this->PostClass(get_adjacent_post( false, "", true ));
+			$this->next = new $this->PostClass(get_adjacent_post( false, "", false ));
 		}
 		return $this->next;
 	}
 
 	function get_prev() {
 		if (!isset($this->prev)){
-			$this->prev = new $this->PostClass(get_adjacent_post( false, "", false ));
+			$this->prev = new $this->PostClass(get_adjacent_post( false, "", true ));
 		}
 		return $this->prev;
 	}
