83e65eb5578f77ff653fb80079a67f39bc1e9e18
Jared Novack <jarednova@upstatement.com>
2013-11-14 20:57:53 -0500

    test for TimberComment filters

--- tests/test-timber-filters.php
+++ tests/test-timber-filters.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 	class TestTimberFilters extends WP_UnitTestCase {
 
 		function testPostMetaFieldFilter(){
@@ -12,6 +12,20 @@
 				$this->assertEquals($timber_post->ID, $post_id);
 			});
 			$this->assertEquals('Drebin', $tp->meta('Frank'));
+		}
+
+		function testCommentMetaFilter(){
+			$post_id = $this->factory->post->create();
+			$comment_id = $this->factory->comment->create(array('comment_post_ID' => $post_id));
+			$comment = new TimberComment($comment_id);
+			$comment->update('ghost', 'busters');
+			add_filter('timber_comment_get_meta_field', function($value, $cid, $field_name, $timber_comment) use ($comment_id){
+				$this->assertEquals($cid, $comment_id);
+				$this->assertEquals($field_name, 'ghost');
+				$this->assertEquals($value, 'busters');
+				$this->assertEquals($comment_id, $timber_comment->ID);
+			});
+			$this->assertEqulas($comment->meta('ghost'), 'busters');
 		}
 
 	}
