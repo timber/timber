d94c5272be41d1dcafb676b0a1c6ffce88695e2f
vagrant <vagrant@vvv.dev>
2013-11-15 02:18:15 +0000

    remove closures from tests

--- tests/test-timber-filters.php
+++ tests/test-timber-filters.php
@@ -6,13 +6,15 @@
 			$post_id = $this->factory->post->create();
 			update_post_meta($post_id, 'Frank', 'Drebin');
 			$tp = new TimberPost($post_id);
-			add_filter('timber_post_get_meta_field', function($value, $pid, $field_name, $timber_post) use ($post_id){
-				$this->assertEquals($field_name, 'Frank');
-				$this->assertEquals($pid, $post_id);
-				$this->assertEquals($timber_post->ID, $post_id);
-				return $value;
-			}, 10, 4);
+			add_filter('timber_post_get_meta_field', array($this, 'filter_timber_post_get_meta_field'), 10, 4);
 			$this->assertEquals('Drebin', $tp->meta('Frank'));
+		}
+
+		function filter_timber_post_get_meta_field($value, $pid, $field_name, $timber_post){
+			$this->assertEquals($field_name, 'Frank');
+			$this->assertEquals($value, 'Drebin');
+			$this->assertEquals($timber_post->ID, $pid);
+			return $value;
 		}
 
 		function testCommentMetaFilter(){
@@ -20,14 +22,15 @@
 			$comment_id = $this->factory->comment->create(array('comment_post_ID' => $post_id));
 			$comment = new TimberComment($comment_id);
 			$comment->update('ghost', 'busters');
-			add_filter('timber_comment_get_meta_field', function($value, $cid, $field_name, $timber_comment) use ($comment_id){
-				$this->assertEquals($cid, $comment_id);
-				$this->assertEquals($field_name, 'ghost');
-				$this->assertEquals($value, 'busters');
-				$this->assertEquals($comment_id, $timber_comment->ID);
-				return $value;
-			}, 10, 4);
+			add_filter('timber_comment_get_meta_field', array($this, 'filter_timber_comment_get_meta_field'), 10, 4);
 			$this->assertEquals($comment->meta('ghost'), 'busters');
+		}
+
+		function filter_timber_comment_get_meta_field($value, $cid, $field_name, $timber_comment){
+			$this->assertEquals($field_name, 'ghost');
+			$this->assertEquals($value, 'busters');
+			$this->assertEquals($cid, $timber_comment->ID);
+			return $value;
 		}
 
 	}
