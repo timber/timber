c0ee6066aa2955f2b387397c1894b6a66f716664
Jared Novack <jarednova@upstatement.com>
2013-11-26 08:00:17 -0600

    looks good! just want to keep indentation clean and open/close braces http://make.wordpress.org/core/2013/11/13/proposed-coding-standards-change-always-require-braces/

--- functions/timber-loader.php
+++ functions/timber-loader.php
@@ -87,11 +87,11 @@ class TimberLoader {
 	function get_locations_theme() {
 		$theme_locs = array();
 		$child_loc = get_stylesheet_directory();
-    $parent_loc = get_template_directory();
-    if (DIRECTORY_SEPARATOR == '\\') {
-      $child_loc = str_replace('/', '\\', $child_loc);
-      $parent_loc = str_replace('/', '\\', $parent_loc);
-    }
+		$parent_loc = get_template_directory();
+		if (DIRECTORY_SEPARATOR == '\\') {
+			$child_loc = str_replace('/', '\\', $child_loc);
+			$parent_loc = str_replace('/', '\\', $parent_loc);
+		}
 		$theme_locs[] = $child_loc;
 		$theme_locs[] = trailingslashit($child_loc) . trailingslashit(Timber::$dirname);
 		if ($child_loc != $parent_loc) {
@@ -101,8 +101,9 @@ class TimberLoader {
 		//now make sure theres a trailing slash on everything
 		foreach ($theme_locs as &$tl) {
 			$tl = trailingslashit($tl);
-      if (DIRECTORY_SEPARATOR == '\\')
-        $tl = str_replace('/', '\\', $tl);
+      		if (DIRECTORY_SEPARATOR == '\\'){
+        		$tl = str_replace('/', '\\', $tl);
+        	}
 		}
 		return $theme_locs;
 	}
