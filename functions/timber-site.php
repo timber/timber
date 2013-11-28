4a28110bef3c8835c49bfc14ae93ce595346344d
Jared Novack <jarednova@upstatement.com>
2013-11-28 14:39:18 -0600

    added non-multisite functions for TimberSite

--- functions/timber-site.php
+++ functions/timber-site.php
@@ -2,14 +2,20 @@
 
 	class TimberSite extends TimberCore {
 		function __construct($site_name_or_id = null){
-			$this->init($site_name_or_id);
+			if (is_multisite()){
+				$this->init_with_multisite($site_name_or_id);
+			} else {
+				$this->init();
+			}
 		}
 
-		function init($site_name_or_id){
+		function init_with_multisite($site_name_or_id){
 			if ($site_name_or_id === null){
 				//this is necessary for some reason, otherwise returns 1 all the time
-				restore_current_blog();
-				$site_name_or_id = get_current_blog_id();
+				if (is_multisite()){
+					restore_current_blog();
+					$site_name_or_id = get_current_blog_id();
+				}
 			}
 			$info = get_blog_details($site_name_or_id);
 			$this->import($info);
@@ -17,9 +23,16 @@
 			$this->name = $this->blogname;
 			$this->title = $this->blogname;
 			$theme_slug = get_blog_option($info->blog_id, 'stylesheet');
-			//echo 'init '.$theme_slug;
 			$this->theme = new TimberTheme($theme_slug);
 			$this->description = get_blog_option($info->blog_id, 'blogdescription');
+		}
+
+		function init(){
+			$this->name = get_bloginfo('name');
+			$this->title = $this->name;
+			$this->description = get_bloginfo('description');
+			$this->url = get_bloginfo('url');
+			$this->language = get_bloginfo('language');
 		}
 
 		function __get($field){
