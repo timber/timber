66be1dc431e5e816218b61d5db8ab0d616f373c8
Jared Novack <jarednova@upstatement.com>
2014-01-02 12:05:37 +0800

    added is_callable to check callback function

--- functions/timber-helper.php
+++ functions/timber-helper.php
@@ -7,8 +7,8 @@ class TimberHelper {
 		if (defined('WP_DISABLE_TRANSIENTS')){
 			$disable_transients = WP_DISABLE_TRANSIENTS;
 		}
-
-		if (false === ($data = get_transient($slug)) || $disable_transients){
+		$data = null;
+		if (is_callable($callback) && (false === ($data = get_transient($slug)) || $disable_transients)){
 			$cache_lock_slug = $slug.'_lock';
 
 			if (get_transient($cache_lock_slug)){
