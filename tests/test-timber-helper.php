4578c80160bbdbf43683fb610711b045c9f81d5f
Jared Novack <jarednova@upstatement.com>
2014-04-10 10:15:15 -0400

    Turns out 3.5.1 has difft HTML for comment form, so simplifying test

--- tests/test-timber-helper.php
+++ tests/test-timber-helper.php
@@ -6,6 +6,6 @@
             $post_id = $this->factory->post->create();
             $form = TimberHelper::get_comment_form($post_id);
             $form = trim($form);
-            $this->assertStringStartsWith('<div id="respond" class="comment-respond">', $form);
+            $this->assertStringStartsWith('<div id="respond"', $form);
         }	
     }
