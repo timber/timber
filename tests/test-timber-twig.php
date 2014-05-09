063a161e5caeb02ddd8e186ee0557377ae49d4e7
Jared Novack <jarednova@upstatement.com>
2014-05-09 14:12:30 -0400

    Updating test for PHP 5.3 limitations

--- tests/test-timber-twig.php
+++ tests/test-timber-twig.php
@@ -4,23 +4,25 @@
 
 		function testDoAction(){
 			global $action_tally;
+			global $php_unit;
+			$php_unit = $this;
 			$action_tally = array();
 			add_action('my_action_foo', function(){
-				global $action_tally;
-				$this->assertTrue(true);
+				global $action_tally, $php_unit;
+				$php_unit->assertTrue(true);
 				$action_tally[] = 'my_action_foo';
 				return 'foo';
 			});
 			add_action('my_action_args', function($bar){
-				global $action_tally;
-				$this->assertEquals('bar', $bar);
+				global $action_tally, $php_unit;
+				$php_unit->assertEquals('bar', $bar);
 				$action_tally[] = 'my_action_args';
 				return 'foo';
 			});
 			add_action('timber_compile_done', function(){
-				global $action_tally;
-				$this->assertContains('my_action_args', $action_tally);
-				$this->assertContains('my_action_foo', $action_tally);
+				global $action_tally, $php_unit;
+				$php_unit->assertContains('my_action_args', $action_tally);
+				$php_unit->assertContains('my_action_foo', $action_tally);
 			});
 			$str = Timber::compile('assets/test-do-action.twig');
 			$str = trim($str);
@@ -28,33 +30,39 @@
 		}
 
 		function testDoActionContext(){
+			global $php_unit;
+			$php_unit = $this;
 			global $action_context_tally;
 			$action_context_tally = array();
 			add_action('my_action_context_vars', function($foo, $bar, $context) {
-				$this->assertEquals('foo', $foo);
-				$this->assertEquals('bar', $bar);
-				$this->assertEquals('Jaredz Post', $context['post']->post_title);
+				global $php_unit;
+				$php_unit->assertEquals('foo', $foo);
+				$php_unit->assertEquals('bar', $bar);
+				$php_unit->assertEquals('Jaredz Post', $context['post']->post_title);
 				global $action_context_tally;
 				$action_context_tally[] = 'my_action_context_vars';
 			}, 10, 3);
 
 			add_action('my_action_context_var', function($foo, $context) {
-				$this->assertEquals('foo', $foo);
-				$this->assertEquals('Jaredz Post', $context['post']->post_title);
+				global $php_unit;
+				$php_unit->assertEquals('foo', $foo);
+				$php_unit->assertEquals('Jaredz Post', $context['post']->post_title);
 				global $action_context_tally;
 				$action_context_tally[] = 'my_action_context_vars';
 			}, 10, 2);
 
 			add_action('my_action_context', function($context){
-				$this->assertEquals('Jaredz Post', $context['post']->post_title);
+				global $php_unit;
+				$php_unit->assertEquals('Jaredz Post', $context['post']->post_title);
 				global $action_context_tally;
 				$action_context_tally[] = 'my_action_context';
 			});
 
 			add_action('timber_compile_done', function(){
+				global $php_unit;
 				global $action_context_tally;
-				$this->assertContains('my_action_context_vars', $action_context_tally);
-				$this->assertContains('my_action_context', $action_context_tally);
+				$php_unit->assertContains('my_action_context_vars', $action_context_tally);
+				$php_unit->assertContains('my_action_context', $action_context_tally);
 			});
 			$post_id = $this->factory->post->create(array('post_title' => "Jaredz Post", 'post_content' => 'stuff to say'));
 			$context['post'] = new TimberPost($post_id);
