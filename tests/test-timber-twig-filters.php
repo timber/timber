6be0ff740efcb4b88786db1905f121776ba4b4f4
Jared Novack <jarednova@upstatement.com>
2014-05-11 21:25:20 -0400

    Passes test for international and normal twig functions

--- tests/test-timber-twig-filters.php
+++ tests/test-timber-twig-filters.php
@@ -20,4 +20,23 @@
 			$str = Timber::compile_string("{{day|date('M jS, Y g:ia')}}", $data);
 			$this->assertEquals('Oct 15th, 2012 8:14pm', trim($str));
 		}
+
+		function testTwigFilterNow(){
+			$now = date('M jS, Y');
+			$str = Timber::compile_string("{{now|date('M jS, Y')}}");
+			$this->assertSame($now, $str);
+			$str = Timber::compile_string("{{null|date('M jS, Y')}}");
+			$this->assertSame($now, $str);
+		}
+
+		function testTwigFilterDateI18n(){
+			//Set to Spanish in wp-config to test
+			//define("WPLANG", "es_ES");
+			if (WPLANG == 'es_ES'){
+				global $wp_locale;
+				$data['day'] = '1983-09-28 20:14:48';
+				$str = Timber::compile_string("{{day|date('F jS, Y g:ia')}}", $data);
+				$this->assertEquals('septiembre 28th, 1983 8:14pm', $str);
+			}
+		}
 	}
