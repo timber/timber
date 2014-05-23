b2882d90ed60f9461411ebb05ee6779a13c78b4c
Jared Novack <jarednova@upstatement.com>
2014-05-23 08:08:25 -0400

    resolved pagination issues

--- tests/test-timber-pagination.php
+++ tests/test-timber-pagination.php
@@ -7,22 +7,31 @@ class TimberPaginationTest extends WP_UnitTestCase {
 		global $wp_rewrite;
 		$wp_rewrite->permalink_structure = false;
 		$posts = $this->factory->post->create_many(55);
-		$this->go_to( site_url( '?s=post' ) );
+		$this->go_to( home_url( '?s=post' ) );
 		$pagination = Timber::get_pagination();
-		$this->assertEquals('http://example.org/?s=post&#038;paged=5', $pagination['pages'][4]['link']);
+		$this->assertEquals(home_url().'/?s=post&#038;paged=5', $pagination['pages'][4]['link']);
 	}
 
-	function testPaginationSearchPretty(){
-		self::run_testPaginationSearchPretty('/%postname%/');
-		self::run_testPaginationSearchPretty('/blog/%year%/%monthnum%/%postname%/');
-	}
-
-	function run_testPaginationSearchPretty($struc = false){
+	function testPaginationSearchPrettyWithPostname(){
+		$struc = '/%postname%/';
 		global $wp_rewrite;
 		$wp_rewrite->permalink_structure = $struc;
 		update_option('permalink_structure', $struc);
 		$posts = $this->factory->post->create_many(55);
-		$this->go_to( site_url( '?s=post' ) );
+		$archive = home_url( '?s=post' );
+		$this->go_to( $archive );
+		$pagination = Timber::get_pagination();
+		$this->assertEquals('http://example.org/page/5/?s=post', $pagination['pages'][4]['link']);
+	}
+
+	function testPaginationSearchPretty(){
+		$struc = '/blog/%year%/%monthnum%/%postname%/';
+		global $wp_rewrite;
+		$wp_rewrite->permalink_structure = $struc;
+		update_option('permalink_structure', $struc);
+		$posts = $this->factory->post->create_many(55);
+		$archive = home_url( '?s=post' );
+		$this->go_to( $archive );
 		$pagination = Timber::get_pagination();
 		$this->assertEquals('http://example.org/page/5/?s=post', $pagination['pages'][4]['link']);
 	}
@@ -32,7 +41,7 @@ class TimberPaginationTest extends WP_UnitTestCase {
 		$wp_rewrite->permalink_structure = $struc;
 		update_option('permalink_structure', $struc);
 		$posts = $this->factory->post->create_many(55);
-		$this->go_to( site_url( '/' ) );
+		$this->go_to( home_url( '/' ) );
 		$pagination = Timber::get_pagination();
 		$this->assertEquals('http://example.org/page/3', $pagination['pages'][2]['link']);
 		$this->assertEquals('http://example.org/page/2', $pagination['next']['link']);
