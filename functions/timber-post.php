905521a1d4c35dce644f4316f5f68f5891143cb0
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-05-24 14:24:31 -0400

    apply_filters expects the original argument

--- functions/timber-post.php
+++ functions/timber-post.php
@@ -439,8 +439,8 @@ class TimberPost extends TimberCore {
      * @return string
      */
     function get_modified_time($time_format = '') {
-        $time_format = $time_format ? $time_format : get_option('time_format');
-        $the_time = get_post_modified_time($time_format, false, $this->ID, true);
+        $tf = $time_format ? $time_format : get_option('time_format');
+        $the_time = get_post_modified_time($tf, false, $this->ID, true);
         return apply_filters('get_the_modified_time', $the_time, $time_format);
     }
 
