a1eb4ccada787b5986dac02de08a4ebcae0718d7
Jared Novack <jarednova@upstatement.com>
2014-05-27 08:24:58 -0400

    added tests for routing

--- tests/test-timber-router.php
+++ tests/test-timber-router.php
@@ -27,4 +27,33 @@ class TimberTestRouter extends WP_UnitTestCase {
 	function testRouterClass(){
 		$this->assertTrue(class_exists('PHPRouter\Router'));
 	}
+
+	function testAppliedRoute(){
+		$_SERVER['REQUEST_METHOD'] = 'GET';
+		global $matches;
+		$matches = array();
+		Timber::add_route('foo', function(){
+			global $matches;
+			$this->assertTrue(true);
+			$matches[] = true;
+		});
+		$this->go_to(home_url('foo'));
+		global $timber;
+		$timber->init_routes();
+		$this->assertEquals(1, count($matches));
+	}
+
+	function testFailedRoute(){
+		$_SERVER['REQUEST_METHOD'] = 'GET';
+		global $matches;
+		$matches = array();
+		Timber::add_route('foo', function(){
+			$this->assertTrue(false);
+			$matches[] = true;
+		});
+		$this->go_to(home_url('bar'));
+		global $timber;
+		$timber->init_routes();
+		$this->assertEquals(0, count($matches));
+	}
 }
