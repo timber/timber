b4b9fdd0e3ac8255b521a8f45bd3fbfd021a1418
Jared Novack <jarednova@upstatement.com>
2014-06-25 22:32:06 -0400

    tests for timber-admin meet medium coverage #264

--- functions/timber-admin.php
+++ functions/timber-admin.php
@@ -1,25 +1,26 @@
 <?php
 
-class TimberAdmin
-{
+class TimberAdmin {
 
 	function __construct() {
-        if (!is_admin()) {
+		if ( !is_admin() ) {
 			return;
 		}
-		add_filter('plugin_action_links', array(&$this, 'settings_link'), 10, 2);
+		add_filter( 'plugin_action_links', array( $this, 'settings_link' ), 10, 2 );
 	}
 
-    /**
-     * @param array $links
-     * @param string $file
-     * @return array
-     */
-    function settings_link($links, $file) {
-		if (strstr($file, '/timber.php')) {
-		    return array_merge(array(
-                'settings' => '<a href="https://github.com/jarednova/timber/wiki" target="_blank">Documentation</a> | <a href="https://github.com/jarednova/timber/wiki/getting-started" target="_blank">Starter Guide</a>'
-            ), $links);
+	/**
+	 *
+	 *
+	 * @param array   $links
+	 * @param string  $file
+	 * @return array
+	 */
+	function settings_link( $links, $file ) {
+		if ( strstr( $file, '/timber.php' ) ) {
+			return array_merge( array(
+					'settings' => '<a href="https://github.com/jarednova/timber/wiki" target="_blank">Documentation</a> | <a href="https://github.com/jarednova/timber/wiki/getting-started" target="_blank">Starter Guide</a> | <a href="http://upstatement.com/timber" target="_blank">Homepage</a>'
+				), $links );
 		}
 		return $links;
 	}
