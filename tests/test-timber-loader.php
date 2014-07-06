7df36fc17c4f8df13b1a3dd94be385e3b00dd3b4
Jared Novack <jarednova@upstatement.com>
2014-07-06 08:39:15 -0400

    remove open_basedir setting after test

--- tests/test-timber-loader.php
+++ tests/test-timber-loader.php
@@ -75,7 +75,7 @@
 		function testTwigLoadsFromAbsolutePathOnServerWithSecurityRestriction(){
 			ini_set('open_basedir', '/srv:/usr:/home/travis/:/tmp:/home:/home/travis/.phpenv/versions/*');
 			$str = Timber::compile('assets/single-foo.twig');
-			ini_set('open_basedir', '/');
+			ini_restore('open_basedir');
 		}
 
 		function testTwigLoadsFromAlternateDirName(){
