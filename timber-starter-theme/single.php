55b3eee08b35ead59b9b754e9f2cc8edcfdd48fe
Andrew Duthie <aduthie@usdigitalpartners.com>
2014-07-08 13:29:29 -0400

    Remove password form from starter theme
    Commit 6e924f9 removes the need for checking post_password_required, as
    attempting to retrieve content will render the password form

--- timber-starter-theme/single.php
+++ timber-starter-theme/single.php
@@ -15,10 +15,4 @@ $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
 $context['comment_form'] = TimberHelper::get_comment_form();
 
-if (post_password_required($post->ID)){
-	Timber::render('single-password.twig', $context);
-} else {
-	Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
-}
-
-
+Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
