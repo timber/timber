a55a3269251cb3248b33c3eee04c62a969e27c58
Andy Ford <andy@fordinteractive.com>
2014-07-16 10:35:34 -0700

    Use WP_SITEURL in 'Timber not activated' error message

--- timber-starter-theme/functions.php
+++ timber-starter-theme/functions.php
@@ -2,7 +2,7 @@
 
 	if (!class_exists('Timber')){
 		add_action( 'admin_notices', function(){
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a></p></div>';
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . _config_wp_siteurl() . '/wp-admin/plugins.php#timber">' . _config_wp_siteurl() . '/wp-admin/plugins.php</a></p></div>';
 		});
 		return;
 	}
