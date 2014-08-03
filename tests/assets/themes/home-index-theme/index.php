82123915a7ee239baac7fd89516aa476ff11d03b
Jared Novack <jared@upstatement.com>
2014-08-03 18:55:13 -0400

    refinement on tests

--- tests/assets/themes/home-index-theme/index.php
+++ tests/assets/themes/home-index-theme/index.php
@@ -2,11 +2,9 @@
 $templates = array( 'index.twig' );
 $context = array();
 if ( is_home() ) {
-	echo 'home';
 	$home = Timber::get_post( 'fake-home' );
 	$context['post'] = $home;
+	$context['content'] = 'from index.php';
 	array_unshift( $templates, 'home.twig' );
-} else {
-	echo 'or not';
 }
 Timber::render( $templates, $context );
