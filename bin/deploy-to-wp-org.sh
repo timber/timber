2ede2ae697917a5a5d21b6f554daee1cef225950
Jared Novack <jared@upstatement.com>
2014-09-04 18:39:25 -0400

    update deploy script

--- bin/deploy-to-wp-org.sh
+++ bin/deploy-to-wp-org.sh
@@ -9,7 +9,7 @@ function deploy () {
 	rm -rf ~/Sites/timber/vendor/dannyvankooten/php-router/.git
 	cd ~/Sites/timber-wp
 	mkdir tags/$1
-	svn add tags/$1
+	
 	cp -r ~/Sites/timber/functions tags/$1/functions
 	cp -r ~/Sites/timber/timber-starter-theme tags/$1/timber-starter-theme
 	cp -r ~/Sites/timber/vendor tags/$1/vendor
@@ -17,6 +17,9 @@ function deploy () {
 	cp ~/Sites/timber/README.md tags/$1/README.md
 	cp ~/Sites/timber/readme.txt tags/$1/readme.txt
 	cp ~/Sites/timber/timber.php tags/$1/timber.php
+	svn add tags/$1
+	cd tags/$1
+	svn commit -m "updating to $1"
 }
 
 deploy $1
