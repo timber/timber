20c6c77104abc3092e66d6189fdc877dd5ccb5f8
Jared Novack <jared@upstatement.com>
2014-09-27 10:24:21 -0500

    added assertions for getting conflicting custom fields

--- tests/test-timber-custom-fields.php
+++ tests/test-timber-custom-fields.php
@@ -16,6 +16,15 @@ class TimberCustomFieldsTest extends WP_UnitTestCase {
 		$str = '{{post.title}}';
 		$result = Timber::compile_string($str, array('post' => $post));
 		$this->assertEquals('foo', $result);
+		//
+		$str = '{{post.post_title}}';
+		update_post_meta($post_id, 'post_title', 'jiggypoof');
+		$post = new TimberPost($post_id);
+		$result = Timber::compile_string($str, array('post' => $post));
+		$this->assertEquals('foo', $result);
+		$str = '{{post.custom.post_title}}';
+		$result = Timber::compile_string($str, array('post' => $post));
+		$this->assertEquals('jiggypoof', $result);
 	}
 
 	function testPostCustomFieldPropertyConflict(){
