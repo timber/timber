21788d555da991c5d0b71f54d7e0345914321d16
Jared Novack <jared@upstatement.com>
2014-12-04 08:20:51 -0500

    ref #402 -- remove link to localhost

--- functions/timber-admin.php
+++ functions/timber-admin.php
@@ -16,7 +16,7 @@ class TimberAdmin {
 	function meta_links( $links, $file ) {
 		if ( strstr( $file, '/timber.php' ) ) {
 			unset($links[2]);
-			$links[] = '<a href="http://localhost/wp-admin/plugin-install.php?tab=plugin-information&amp;plugin=timber-library&amp;TB_iframe=true&amp;width=600&amp;height=550" class="thickbox" aria-label="More information about Timber" data-title="Timber">View details</a>';
+			$links[] = '<a href="/wp-admin/plugin-install.php?tab=plugin-information&amp;plugin=timber-library&amp;TB_iframe=true&amp;width=600&amp;height=550" class="thickbox" aria-label="More information about Timber" data-title="Timber">View details</a>';
 			$links[] = '<a href="http://upstatement.com/timber" target="_blank">Homepage</a>';
 			$links[] = '<a href="https://github.com/jarednova/timber/wiki" target="_blank">Documentation</a>';
 			$links[] = '<a href="https://github.com/jarednova/timber/wiki/getting-started" target="_blank">Starter Guide</a>';
