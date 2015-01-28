680e4b48083a52626d45a4e293058fd4753cf86a
kuus <kunderikuus@gmail.com>
2015-01-28 19:40:42 +0100

    style(starter-theme) adhere to WordPress coding standard in php files (resolve jarednova/timber#454)

--- timber-starter-theme/404.php
+++ timber-starter-theme/404.php
@@ -9,5 +9,5 @@
  * @since    Timber 0.1
  */
 
-	$context = Timber::get_context();
-	Timber::render('404.twig', $context);
+$context = Timber::get_context();
+Timber::render( '404.twig', $context );
--- timber-starter-theme/index.php
+++ timber-starter-theme/index.php
@@ -13,17 +13,15 @@
  * @since 		Timber 0.1
  */
 
-	if (!class_exists('Timber')){
-		echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-		return;
-	}
-	$context = Timber::get_context();
-	$context['posts'] = Timber::get_posts();
-	$context['foo'] = 'bar';
-	$templates = array('index.twig');
-	if (is_home()){
-		array_unshift($templates, 'home.twig');
-	}
-	Timber::render($templates, $context);
-
-
+if ( ! class_exists( 'Timber' ) ) {
+	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+	return;
+}
+$context = Timber::get_context();
+$context['posts'] = Timber::get_posts();
+$context['foo'] = 'bar';
+$templates = array('index.twig');
+if ( is_home() ) {
+	array_unshift( $templates, 'home.twig' );
+}
+Timber::render( $templates, $context );
