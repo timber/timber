134b2a29c8649a64067d0346fc714db7e5ce637f
Jared Novack <jared@upstatement.com>
2015-04-14 14:07:22 -0400

    ref #519 -- allow for composer stuff in child or parent theme

--- timber.php
+++ timber.php
@@ -13,10 +13,12 @@ global $timber;
 
 // we look for Composer files first in the plugins dir
 // then in the wp-content dir (site install)
-// and finally in the current themes directory
-if (   file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' )
-	|| file_exists( $composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php')
-	|| file_exists( $composer_autoload = get_template_directory().'/vendor/autoload.php')) {
+// and finally in the current themes directories
+if (   file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
+	|| file_exists( $composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php') /* check in wp-content */
+	|| file_exists( $composer_autoload = get_stylesheet_directory().'/vendor/autoload.php') /* check in child theme */
+	|| file_exists( $composer_autoload = get_template_directory().'/vendor/autoload.php') /* check in parent theme */
+	) {
 	require_once $composer_autoload;
 }
 
