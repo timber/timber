bcb09739d745c25daebbcd7f10522ccd0d6b5937
Jared Novack <jared@upstatement.com>
2015-07-15 15:03:45 -0500

    adding additional load path

--- timber.php
+++ timber.php
@@ -16,6 +16,7 @@ global $timber;
 // and finally in the current themes directories
 if (   file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
 	|| file_exists( $composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php') /* check in wp-content */
+	|| file_exists( $composer_autoload = plugin_dir_path( __FILE__ ).'vendor/autoload.php') /* check in plugin directory */
 	|| file_exists( $composer_autoload = get_stylesheet_directory().'/vendor/autoload.php') /* check in child theme */
 	|| file_exists( $composer_autoload = get_template_directory().'/vendor/autoload.php') /* check in parent theme */
 	) {
