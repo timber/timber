ea7082bbc8d3df90987af2662f69a4426f9cbfd7
Jared <jared@Jareds-MacBook-Air.local>
2015-08-11 20:58:14 -0400

    ref #607 -- trying markdowndocs instead

--- bin/generate-docs.sh
+++ bin/generate-docs.sh
@@ -1,6 +1,9 @@
-phpdoc --template="xml"
-mkdir /srv/www/timber/docs/markdown
-vendor/evert/phpdoc-md/bin/phpdocmd docs/output/structure.xml /srv/www/timber/docs/markdown
+#phpdoc --template="xml"
+#mkdir /srv/www/timber/docs/markdown
+#vendor/evert/phpdoc-md/bin/phpdocmd docs/output/structure.xml /srv/www/timber/docs/markdown
+mkdir docs/markdowndocs
+./vendor/victorjonsson/markdowndocs/bin/phpdoc-md generate TimberPost > docs/markdowndocs/index.md
+
 cd /srv/www/slate
 grunt
 mv /srv/www/timber/docs/markdown/*.md /srv/www/slate/source/includes
