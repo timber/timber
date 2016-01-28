92c8fc75f799d38f35af2998c96f02a8b3f4c471
Moritz Kornher <moritz@prefer.co.nz>
2016-01-29 12:40:09 +1300

    Readded the WordPress plugin file with original content (autloading, instantiation for Timber object)

---
+++ timber.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Timber
+Description: The WordPress Timber Library allows you to write themes using the power Twig templates.
+Plugin URI: http://timber.upstatement.com
+Author: Jared Novack + Upstatement
+Version: 0.22.5
+Author URI: http://upstatement.com/
+*/
+
+global $wp_version;
+global $timber;
+
+// we look for Composer files first in the plugins dir.
+// then in the wp-content dir (site install).
+// and finally in the current themes directories.
+if (   file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
+    || file_exists( $composer_autoload = WP_CONTENT_DIR.'/vendor/autoload.php') /* check in wp-content */
+    || file_exists( $composer_autoload = plugin_dir_path( __FILE__ ).'vendor/autoload.php') /* check in plugin directory */
+    || file_exists( $composer_autoload = get_stylesheet_directory().'/vendor/autoload.php') /* check in child theme */
+    || file_exists( $composer_autoload = get_template_directory().'/vendor/autoload.php') /* check in parent theme */
+) {
+    require_once $composer_autoload;
+}
+
+$timber = new Timber();
+Timber::$dirname = 'views';
