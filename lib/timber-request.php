d54a6b73bc588266d02780ed25cefb012aec1a9f
Connor Burton <connorjburton@gmail.com>
2016-02-17 13:59:40 +0000

    Adding required interface functions

--- lib/timber-request.php
+++ lib/timber-request.php
@@ -1,10 +1,8 @@
 <?php
-
 /**
  * TimberRequest exposes $_GET and $_POST to the context
  */
 class TimberRequest extends TimberCore implements TimberCoreInterface {
-
 	public $post = array();
 	public $get = array();
 	
@@ -15,7 +13,6 @@ class TimberRequest extends TimberCore implements TimberCoreInterface {
 	function __construct() {
 		$this->init();
 	}
-
 	/**
 	 * @internal
 	 */
@@ -23,4 +20,15 @@ class TimberRequest extends TimberCore implements TimberCoreInterface {
 		$this->post = $_POST;
 		$this->get = $_GET;
 	}
+
+	public function __call( $field, $args ) {}
+
+	public function __get( $field ) {}
+
+	/**
+	 * @return boolean
+	 */
+	public function __isset( $field ) {}
+
+	public function meta( $key ) {}
 }
