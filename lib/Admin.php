26b6585103c80c2262495087823fbc43241a95dc
Jared <jared@Jareds-MacBook-Pro.local>
2016-05-03 06:33:18 -0400

    Fixed some last issues in 1.0 upgrade

--- lib/Admin.php
+++ lib/Admin.php
@@ -25,6 +25,7 @@ class Admin {
 			$links[] = '<a href="http://upstatement.com/timber" target="_blank">Homepage</a>';
 			$links[] = '<a href="https://github.com/timber/timber/wiki" target="_blank">Documentation</a>';
 			$links[] = '<a href="https://github.com/timber/timber/wiki/getting-started" target="_blank">Starter Guide</a>';
+			$links[] = '<p><a href="https://downloads.wordpress.org/plugin/timber-library.0.22.6.zip">Site not working on 1.0? Downgrade to version 0.22.6</a></p>';
 			return $links;
 		}
 		return $links;
@@ -43,27 +44,26 @@ class Admin {
 	 *  @param	{object}	$r
 	 */
 	function in_plugin_update_message( $plugin_data, $r ) {
+		$m = '';
 
-		
-		//print_r($r);
-		// vars
-		if ( version_compare("1.0", $plugin_data->Version) > 0 ) {
-			$m = '<p><b>Warning:</b> Timber 1.0 removed a number of features and methods. Before upgrading please test your theme on a local or staging site to ensure that your theme will work with the newest version.</p> 
+		if ( version_compare("1.0.0", $plugin_data->new_version) <= 0 ) {
+			//a version of 1.0.0 or greater is availalbe
+			$m .= '<p><b>Warning:</b> Timber 1.0 removed a number of features and methods. Before upgrading please test your theme on a local or staging site to ensure that your theme will work with the newest version.</p> 
 
 			<p><strong>Is your theme in active development?</strong> That is, is someone actively in PHP files writing new code? If you answered "no", then <i>do not upgrade</i>. You will not benefit from Timber 1.0</p>';
 
 			$m .= '<p>Read the <strong><a href="https://github.com/timber/timber/wiki/1.0-Upgrade-Guide">Upgrade Guide</a></strong> for more information</p>';
 
-			if ( version_compare("0.22.6", $plugin_data->Version) > 0 ) {
-				$m .= "<p>You can also <b><a href='https://downloads.wordpress.org/plugin/timber-library.0.22.6.zip'>upgrade to version 0.22.6</a></b> if you want to upgrade, but are unsure if you're ready for 1.0";
-			}
+			$m .= "<p>You can also <b><a href='https://downloads.wordpress.org/plugin/timber-library.0.22.6.zip'>upgrade to version 0.22.6</a></b> if you want to upgrade, but are unsure if you're ready for 1.0";
+
 		}
 
-		
-		
+		if ( version_compare("1.0.0", $plugin_data->Version) <= 0 ) {
+			$m .= "<p>Are you seeing errors since upgrading to 1.0? Download <b><a href='https://downloads.wordpress.org/plugin/timber-library.0.22.6.zip'>Version 0.22.6</a></b> to bring things back to stability.";
+		}
 		
 		// show message
-		echo '<br />'.sprintf($m, admin_url('edit.php?post_type=acf-field-group&page=acf-settings-updates'), 'http://www.advancedcustomfields.com/pro');
+		echo '<br />'.sprintf($m);
 	
 	}
 
