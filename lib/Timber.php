4ab02bf4c799ef0b2d4f6f88a74afe4fa09c3068
Jared Novack <jared@upstatement.com>
2016-05-19 06:16:41 -0400

    Update Timber.php

--- lib/Timber.php
+++ lib/Timber.php
@@ -32,7 +32,7 @@ use Timber\Loader;
  */
 class Timber {
 
-	public static $version = '1.0.2';
+	public static $version = '1.0.3';
 	public static $locations;
 	public static $dirname;
 	public static $twig_cache = false;
