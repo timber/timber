f09e8f52b1e2c6a21fd3507eb2d70777ffe19adc
Jared Novack <jarednova@upstatement.com>
2016-05-21 16:03:36 -0400

    ref #65 -- test for stripping of tags

--- tests/test-timber-post-preview-object.php
+++ tests/test-timber-post-preview-object.php
@@ -19,7 +19,7 @@
 		}
 
 		function testPostPreviewObject() {
-			$pid = $this->factory->post->create( array('post_content' => 'Great Gatsby', 'post_excerpt' => 'In my younger and more vulnerable years my father gave me some advice that I’ve been turning over in my mind ever since.') );
+			$pid = $this->factory->post->create( array('post_content' => 'Great Gatsby', 'post_excerpt' => 'In my younger and more vulnerable years my father gave me some advice that I’ve been <a href="http://google.com">turning over</a> in my mind ever since.') );
 			$template = '{{ post.preview }}';
 			$post = new TimberPost($pid);
 			$str = Timber::compile_string($template, array('post' => $post));
