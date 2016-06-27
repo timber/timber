987cdb32db01e0bb9a0d58f538fbd5ab0b20bac1
Linda Gorman <lggorman1@gmail.com>
2016-06-27 10:14:52 -0400

    Removed option to include password

--- lib/User.php
+++ lib/User.php
@@ -67,10 +67,9 @@ class User extends Core implements CoreInterface {
 
 	/**
 	 * @param int|bool $uid
-	 * @param bool $include_pass Whether to include the user's hashed password
 	 */
-	public function __construct( $uid = false, $include_pass = false ) {
-		$this->init($uid, $include_pass);
+	public function __construct( $uid = false ) {
+		$this->init($uid);
 	}
 
 	/**
@@ -119,9 +118,8 @@ class User extends Core implements CoreInterface {
 	/**
 	 * @internal
 	 * @param int|bool $uid The user ID to use
-	 * @param bool $include_pass Whether to include the user's hashed password
 	 */
-	protected function init( $uid = false, $include_pass = false ) {
+	protected function init( $uid = false ) {
 		if ( $uid === false ) {
 			$uid = get_current_user_id();
 		}
@@ -144,9 +142,7 @@ class User extends Core implements CoreInterface {
 				$this->import($data);
 			}
 		}
-		if ( ! $include_pass ) {
-			unset( $this->user_pass );
-		}
+		unset($this->user_pass);
 		$this->id = $this->ID;
 		$this->name = $this->name();
 		$this->avatar = new Image(get_avatar_url($this->id));
--- tests/test-timber-user.php
+++ tests/test-timber-user.php
@@ -27,16 +27,10 @@
 			$this->assertEquals('Sixteenth President', $str);
 		}
 
-		function testInitWithoutPasswordFlag() {
+		function testInitShouldUnsetPassword() {
 			$uid = $this->factory->user->create(array('display_name' => 'Tom Riddle'));
 			$user = new TimberUser($uid );
 			$this->assertFalse(property_exists( $user, 'user_pass'));
-		}
-
-		function testInitWithPasswordFlag() {
-			$uid = $this->factory->user->create(array('display_name' => 'Serius Black'));
-			$user = new TimberUser($uid, true);
-			$this->assertTrue(property_exists($user, 'user_pass'));
 		}
 
 		function testInitWithObject(){
