82365d5b83f9583e21848f79314c0b2b8680603f
Jared Novack <jarednova@upstatement.com>
2016-07-23 18:26:46 -0400

    ref #1059 -- switching location of PHP file to work with both Travis and local

--- tests/test-timber-debug.php
+++ tests/test-timber-debug.php
@@ -5,7 +5,7 @@ class TestTimberDebug extends Timber_UnitTestCase {
 
 	function testCallingPHPFile() {
 		add_filter('timber/calling_php_file', function($file) {
-			$this->assertEquals('/srv/www/timber/tests/test-timber-debug.php', $file);
+			$this->assertStringEndsWith('/timber/tests/test-timber-debug.php', $file);
 		});
 		Timber::compile('assets/output.twig');
 
