4e819a66ff2c81b668a9d9164cdbcd02102825b0
Josh Levinson <joshalevinson@gmail.com>
2016-10-24 15:57:25 -0400

    Change priority of Class Factory so that function arguments take precedence Previously, the function argument for the class was only considered if the Class Map filter didn't yield a valid class This changes the logic so that if a class is specified, it takes priority The final is still filterable via `Timber\[Post|Category]ClassMap`

--- lib/Factory/ObjectClassFactory.php
+++ lib/Factory/ObjectClassFactory.php
@@ -16,7 +16,7 @@ class ObjectClassFactory {
 	 * @param string $type The object type–"post" or "term"
 	 * @param string|null $object_type The object's internal type–a post type or taxonomy
 	 * @param object|null $object The object for which the class is being retrieved
-	 * @param string|null $class The desired "default" class
+	 * @param string|null $class The desired class; overrides results of Timber\${type}ClassMap filter
 	 *
 	 * @return mixed|string
 	 */
@@ -26,20 +26,16 @@ class ObjectClassFactory {
 
 		$class_to_use = $default_class = static::${"{$type}Class"};
 
-		if ( ! $class ) {
-			$class = $default_class;
-		}
-
 		$class = apply_filters( "Timber\\${type}ClassMap", $class, $object_type, $object, $default_class );
 
-		if ( is_array( $class ) && is_string( $object_type ) ) {
+		if ( is_string( $class ) ) {
+			$class_to_use = $class;
+		} else if ( is_array( $class ) && is_string( $object_type ) ) {
 			if ( isset( $class[ $object_type ] ) ) {
 				$class_to_use = $class[ $object_type ];
 			} else {
 				Helper::error_log( $object_type . ' not found in ' . print_r( $class, true ) );
 			}
-		} elseif ( is_string( $class ) ) {
-			$class_to_use = $class;
 		} else {
 			Helper::error_log( "Unexpected value for {$type}Class: " . print_r( $class, true ) );
 		}
@@ -48,7 +44,7 @@ class ObjectClassFactory {
 			Helper::error_log( 'Class ' . $class_to_use . " either does not exist or implement $default_class" );
 		}
 
-		return $class_to_use;
+		return apply_filters( 'Timber\PostClass', $class_to_use, $object_type, $object_type, $default_class );
 	}
 
 }
