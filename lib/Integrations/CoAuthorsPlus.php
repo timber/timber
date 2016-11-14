0a73158fc28ffd7d428c581fd6b6b103a62e0d74
Mouti'a Benachour <motie.benachour@gmail.com>
2016-11-14 14:47:43 +0100

    refactoring and tests to coauthors plus integration

--- lib/Integrations/CoAuthorsPlus.php
+++ lib/Integrations/CoAuthorsPlus.php
@@ -5,7 +5,6 @@ namespace Timber\Integrations;
 class CoAuthorsPlus {
 
 	public static $prefer_gravatar = false;
-	public static $prefer_user = false;
 	/**
 	 * @codeCoverageIgnore
 	 */
@@ -24,12 +23,37 @@ class CoAuthorsPlus {
 		$authors = array();
 		$cauthors = get_coauthors($post->ID);
 		foreach ( $cauthors as $author ) {
-			if( is_object($author) && isset($author->type) && $author->type == 'guest-author' ){
-				$authors[] = new CoAuthorsPlusUser($author);
+			$uid = $this->get_user_uid( $author );
+			if($uid){
+				$authors[] = new \Timber\User($uid);
 			} else {
-				$authors[] = new \Timber\User($author);
+				$authors[] = new CoAuthorsPlusUser($author);
 			}
 		}
 		return $authors;
+	}
+
+	/**
+	 * return the user id for normal authors
+	 * the user login for guest authors if it exists and self::prefer_users == true
+	 * or null
+	 * @internal
+	 * @param object $cauthor
+	 * @return int|string|null
+	 */
+	protected function get_user_uid( $cauthor )
+	{
+		// if is guest author
+		if( is_object($cauthor) && isset($cauthor->type) && $cauthor->type == 'guest-author'){
+			// if have have a linked user account
+			global $coauthors_plus;
+			if( !$coauthors_plus->force_guest_authors && isset($cauthor->linked_account) && !empty($cauthor->linked_account ) ){
+				return $cauthor->linked_account;
+			} else {
+				return null;
+			}
+		} else {
+			return $cauthor->id;
+		}
 	}
 }
--- lib/Integrations/CoAuthorsPlusUser.php
+++ lib/Integrations/CoAuthorsPlusUser.php
@@ -4,55 +4,38 @@ namespace Timber\Integrations;
 
 class CoAuthorsPlusUser extends \Timber\User
 {
+	/**
+	 * @param object $author co-author object
+	 */
 	public function __construct( $author )
 	{
-		$uid = $this->get_coauthor_uid($author);
-		parent::__construct($uid);
+		parent::__construct($author);
 	}
 
 	/**
 	 * @internal
-	 * @param false|object|string $coauthor co-author object
+	 * @param false|object $coauthor co-author object
 	 */
 	protected function init( $coauthor = false ){
-		if ( is_object($coauthor) ){
-			$this->id = $coauthor->ID;
-			$this->first_name = $coauthor->first_name;
-			$this->last_name = $coauthor->last_name;
-			$this->user_nicename = $coauthor->user_nicename;
-			$this->description = $coauthor->description;
+		$this->id = $coauthor->ID;
+		$this->first_name = $coauthor->first_name;
+		$this->last_name = $coauthor->last_name;
+		$this->user_nicename = $coauthor->user_nicename;
+		$this->description = $coauthor->description;
 
-			/**
-			 * @property string name
-			 */
-			$this->name = $coauthor->display_name;
-			$this->_link = get_author_posts_url(null, $coauthor->user_nicename );
+		/**
+		 * @property string name
+		 */
+		$this->name = $coauthor->display_name;
+		$this->_link = get_author_posts_url(null, $coauthor->user_nicename );
 
-			// 96 is the default wordpress avatar size
-			$avatar_url = get_the_post_thumbnail_url($this->id, 96);
-			if( CoAuthorsPlus::$prefer_gravatar || !$avatar_url ){
-				$avatar_url = get_avatar_url($coauthor->user_email);
-			}
-			if( $avatar_url ){
-				$this->avatar = new \Timber\Image($avatar_url);
-			}
-		} else {
-			parent::init($coauthor);
+		// 96 is the default wordpress avatar size
+		$avatar_url = get_the_post_thumbnail_url($this->id, 96);
+		if( CoAuthorsPlus::$prefer_gravatar || !$avatar_url ){
+			$avatar_url = get_avatar_url($coauthor->user_email);
 		}
-	}
-
-	/**
-	 * return $uid used by init method
-	 * @internal
-	 * @param object $cauthor
-	 * @return string|object
-	 */
-	protected function get_coauthor_uid( $cauthor )
-	{
-		if( CoAuthorsPlus::$prefer_user && isset($cauthor->linked_account) && !empty($cauthor->linked_account ) ){
-			return $cauthor->linked_account;
-		} else {
-			return $cauthor;
+		if( $avatar_url ){
+			$this->avatar = new \Timber\Image($avatar_url);
 		}
 	}
 }
