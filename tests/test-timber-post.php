ebd6428352289d5ec0c0d03f184722ce072bf28b
Jared Novack <jarednova@upstatement.com>
2017-01-25 09:02:49 -0500

    ref #1296 -- added print_r for some debugging

--- tests/test-timber-post.php
+++ tests/test-timber-post.php
@@ -560,7 +560,9 @@
 			// Uncategorized is applied by default
 			$default_categories = $post->categories();
 			$this->assertEquals('uncategorized', $default_categories[0]->slug);
-
+			echo '=== $category_names ===';
+			print_r($category_names);
+			exit;
 			foreach ( $category_names as $category_name ) {
 				$category_name = wp_insert_term($category_name, 'category');
 				wp_set_object_terms($pid, $category_name['term_id'], 'category', true);
