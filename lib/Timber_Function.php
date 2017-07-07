5970c565c4dc18cbaf29815e7de6cb4f7e6b0fbb
Luís Silva <luismiguel.s91@gmail.com>
2017-07-07 22:39:36 +0100

    extend Timber_Function from Twig_Function or Twig_SimpleFunction, depending on Twig version

--- lib/Timber_Function.php
+++ lib/Timber_Function.php
@@ -2,6 +2,12 @@
 
 namespace Timber;
 
-class Timber_Function extends \Twig_Function {
+if ( version_compare(\Twig_Environment::VERSION, '2.0.0', '>=') ) {
 
+  class Timber_Function extends \Twig_Function { }
+
+} else {
+
+  class Timber_Function extends \Twig_SimpleFunction { }
+  
 }
