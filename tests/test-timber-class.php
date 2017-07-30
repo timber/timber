0c06acbd6482263de24f3cc262d0bd915beeedf9
Heino H. Gehlsen <heino@gehlsen.dk>
2017-07-30 19:37:44 +0200

    Remove unit test of no private and static method Timber::init_constants()

--- tests/test-timber-class.php
+++ tests/test-timber-class.php
@@ -2,11 +2,5 @@
 
 class TestTimberClass extends Timber_UnitTestCase {
 
-	function testConstantsDefining() {
-		$timber = $GLOBALS['timber'];
-		$timber->init_constants();
-		$timber->init_constants();
-		/* just testing to make sure the double call doesnt error-out */
-	}
 
 }
