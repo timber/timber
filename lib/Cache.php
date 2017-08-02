39515f6c34b078845fea45b2563395fa9c5d459e
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-03 00:56:29 +0200

    Move methods from Cache\Cleaner into Cache; leave public method as a facade for backward compatibility

--- lib/Cache.php
+++ lib/Cache.php
@@ -25,7 +25,7 @@ final class Cache
 
 	public static function deleteCache()
 	{
-		Cleaner::delete_transients();
+		Self::deleteTransients();
 	}
 
 	public static function clearCacheTimber( $cache_mode = self::CACHE_USE_DEFAULT )
@@ -190,4 +190,76 @@ final class Cache
 		return $cache_mode;
 	}
 
+	public static function deleteTransients()
+	{
+		global $_wp_using_ext_object_cache;
+
+		if ( $_wp_using_ext_object_cache ) {
+			return 0;
+		}
+
+		global $wpdb;
+		$records = 0;
+
+		// Delete transients from options table
+		$records .= self::deleteTransientsSingleSite();
+
+		// Delete transients from multisite, if configured as such
+
+		if ( is_multisite() && is_main_network() ) {
+
+			$records .= self::deleteTransientsMultisite();
+		}
+		return $records;
+
+	}
+	protected static function deleteTransientsSingleSite()
+	{
+		global $wpdb;
+		$sql = "
+				DELETE
+					a, b
+				FROM
+					{$wpdb->options} a, {$wpdb->options} b
+				WHERE
+					a.option_name LIKE '%_transient_%' AND
+					a.option_name NOT LIKE '%_transient_timeout_%' AND
+					b.option_name = CONCAT(
+						'_transient_timeout_',
+						SUBSTRING(
+							a.option_name,
+							CHAR_LENGTH('_transient_') + 1
+						)
+					)
+				AND b.option_value < UNIX_TIMESTAMP()
+			";
+
+		return $wpdb->query($sql);
+	}
+
+	protected static function deleteTransientsMultisite()
+	{
+		global $wpdb;
+		$sql = "
+				DELETE
+					a, b
+				FROM
+					{$wpdb->sitemeta} a, {$wpdb->sitemeta} b
+				WHERE
+					a.meta_key LIKE '_site_transient_%' AND
+					a.meta_key NOT LIKE '_site_transient_timeout_%' AND
+					b.meta_key = CONCAT(
+						'_site_transient_timeout_',
+						SUBSTRING(
+							a.meta_key,
+							CHAR_LENGTH('_site_transient_') + 1
+						)
+					)
+				AND b.meta_value < UNIX_TIMESTAMP()
+			";
+
+		$clean = $wpdb->query($sql);
+
+		return $clean;
+	}
 }
