69e8047cb1f8228124877862c147c67381942839
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-02 12:58:24 +0200

    Move filter from Twig_Environment.php to Timber.php

--- lib/Timber.php
+++ lib/Timber.php
@@ -724,3 +724,28 @@ class Timber {
 
 
 }
+
+/**
+ * @param \Twig_Environment $twig
+ * @return \Twig_Environment
+ * @internal
+ */
+function do_legacy_twig_environment_filters_pre_timber_twig(\Twig_Environment $twig) {
+	do_action('twig_apply_filters', $twig);
+	do_action('timber/twig/filters', $twig);
+}
+// Attach action with lower than default priority to simulate the filters prior location before 'timber/twig' was fired at the bottom of Twig::add_timber_filters()
+add_action('timber/twig', __NAMESPACE__.'\do_legacy_twig_environment_filters_pre_timber_twig', 5);
+
+/**
+ * @param \Twig_Environment $twig
+ * @return \Twig_Environment
+ * @internal
+ */
+function do_legacy_twig_environment_filters_post_timber_twig(\Twig_Environment $twig) {
+	do_action('timber/twig/functions', $twig);
+	do_action('timber/twig/escapers', $twig);
+	do_action('timber/loader/twig', $twig);
+}
+// Attach action with higher than default priority to simulate the filters prior location after 'timber/twig' was fired at the bottom of Twig::add_timber_filters()
+add_action('timber/twig', __NAMESPACE__.'\do_legacy_twig_environment_filters_post_timber_twig', 15);
