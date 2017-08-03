8bb965450bb8f3c6119f4a7bda2a9f19d3bd1eb0
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-03 12:35:23 +0200

    No point in just sending the method's own default value

--- lib/Timber.php
+++ lib/Timber.php
@@ -484,7 +484,7 @@ class Timber {
 				$key = md5($name.json_encode($context));
 
 				// Load cached output
-				$output = Cache::fetch($key, $cache_mode, Cache::CACHEGROUP);
+				$output = Cache::fetch($key, $cache_mode);
 			}
 
 			// If no output at this point, generate some...
@@ -517,7 +517,7 @@ class Timber {
 				// Erase cache
 				Cache::deleteCache();
 				// Store output
-				Cache::save($key, $output, $expires, $cache_mode, Cache::CACHEGROUP);
+				Cache::save($key, $output, $expires, $cache_mode);
 			}
 //
 // Content from moved Loader::render() ends here.
