0a8f928bb96f2c877b2801c729ea977962aac8fc
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-03 16:56:29 +0200

    Add clearTimber() from Cache class

--- lib/Cache/Psr16/WordpressObjectCachePool.php
+++ lib/Cache/Psr16/WordpressObjectCachePool.php
@@ -356,4 +356,19 @@ class WordpressObjectCachePool
 		//
 		return true;
 	}
+
+	public static function clearTimber()
+	{
+// Origin: Timber v1.3.4 (Timber\Loader::clear_cache_timber_object())
+		global $wp_object_cache;
+		if ( isset($wp_object_cache->cache[$this->group]) ) {
+			$items = $wp_object_cache->cache[$this->group];
+			foreach ( $items as $key => $value ) {
+				if ( is_multisite() ) {
+					$key = preg_replace('/^(.*?):/', '', $key);
+				}
+				wp_cache_delete($key, $this->group);
+			}
+			return true;
+		}
 }
