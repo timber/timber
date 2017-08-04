9141b73541e0db9de4869e06aa6205b07fbdaa24
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-04 11:00:51 +0200

    Make classes support adding a prefix to the key

--- lib/Cache/Psr16/TimberTransientPool.php
+++ lib/Cache/Psr16/TimberTransientPool.php
@@ -13,9 +13,136 @@ namespace Timber\Cache\Psr16;
 class TimberTransientPool
 	extends WordpressTransientPool
 {
-	public static function clearTimber($group = \Timber\Cache::CACHEGROUP)
+	/**
+     * @var string
+     */
+	protected $keyPrefix;
+
+	/**
+     * @param string @group
+     */
+    public function __construct($prefix = null, $separator = '_')
+	{
+		// Verify that Wordpress is loaded.
+		parent::__construct();
+
+		//
+		$this->validateGroup($prefix);
+
+		//
+		$this->keyPrefix = $prefix == null ? '' : $prefix.$separator;
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function get($key, $default = null)
+	{
+		dump($this->keyPrefix.$key);
+		return parent::get($this->keyPrefix.$key, $default);
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function set($key, $value, $ttl = null)
+	{
+		return parent::set($this->keyPrefix.$key, $value, $ttl);
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+	{
+		return parent::delete($this->keyPrefix.$key);
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function getMultiple($keys, $default = null)
+	{
+		//
+		$keyPrefix = $this->keyPrefix;
+		
+		//
+		$keys = array_map(
+			function($key) use ($keyPrefix) {
+				return $keyPrefix.$key;
+			}, 
+			$keys
+		);
+
+		//
+		return parent::deleteMultiple($values, $default);
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function setMultiple($values, $ttl = null)
+	{
+		//
+		$keyPrefix = $this->keyPrefix;
+
+		//
+		$values = array_combine(
+			array_map(
+				function($key) use ($keyPrefix) {
+					return $keyPrefix.$key;
+				},
+				array_keys($values)
+			),
+			$values
+		);
+		
+		//
+		return parent::setMultiple($values, $ttl);
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function deleteMultiple($keys)
+	{
+		//
+		$keyPrefix = $this->keyPrefix;
+		
+		//
+		$keys = array_map(
+			function($key) use ($keyPrefix) {
+				return $keyPrefix.$key;
+			}, 
+			$keys
+		);
+
+		//
+		return parent::deleteMultiple($values);
+	}
+
+	/**
+     * {@inheritdoc}
+     */
+    public function has($key)
+	{
+		return parent::has($this->keyPrefix.$key);
+	}
+
+    /**
+     * 
+     */
+    protected function validateGroup($prefix)
+	{
+		// TODO: Thow something on unwated stuff...
+	}
+
+	public static function clearTimber($prefix = \Timber\Cache::CACHEGROUP, $separator = '_')
 	{
 // Origin: Timber v1.3.4 (Timber\Loader::clearCacheTimberDatabase())
+
+// TODO: Change to non-static mathod, at get prefix from $this->keyPrefix.
+// TODO: This is SQL! $group and $separator is currently unsafe!
 		global $wpdb;
 		$query = $wpdb->prepare(
 			"DELETE
@@ -24,7 +151,7 @@ class TimberTransientPool
 				WHERE
 					option_name
 				LIKE '%s'",
-			'_transient_'.$group.'_%'
+			'_transient_'.$prefix.$separator.'%'
 		);
 		return $wpdb->query($query);
 	}
