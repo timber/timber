efb6ffed265a1558c7f8d57f2a7f1a90a85509cf
Heino H. Gehlsen <heino@gehlsen.dk>
2017-08-04 20:52:03 +0200

    Make Asm89SimpleCacheAdapter exception safe

--- lib/Cache/Psr16/Asm89SimpleCacheAdapter.php
+++ lib/Cache/Psr16/Asm89SimpleCacheAdapter.php
@@ -20,6 +20,11 @@ class Asm89SimpleCacheAdapter
     private $cache;
 
     /**
+     * @var \Psr\SimpleCache\CacheException
+     */
+    private $lastException;
+
+    /**
      * @param \Psr\SimpleCache\CacheInterface $cache
      */
     public function __construct(\Psr\SimpleCache\CacheInterface $cache)
@@ -29,21 +34,68 @@ class Asm89SimpleCacheAdapter
 
     /**
      * @param string $key
-     * @return mixed|false
+     * @return mixed|false False, if there was no value to be fetched. Null or a string otherwise.
      */
     public function fetch($key)
     {
-		return $this->cache->get($key);
+		// Reset last exception
+		$this->lastException = null;
+		
+		try {
+			// Get value from implementing library. Return false if there was no value to be fetched
+			$value = $this->cache->get($key, false);
+			
+			// Return cached value
+			return $value;
+
+		// "Exception interface for invalid cache arguments."
+		} catch (\Psr\SimpleCache\InvalidArgumentException $e) {
+			// Save exception for retrieval by caller 
+			$this->lastException = $e;
+			// Return fail to caller
+			return false;
+
+		// "Interface used for all types of exceptions thrown by the implementing library."
+		} catch (\Psr\SimpleCache\CacheException $e) {
+			// Save exception for retrieval by caller 
+			$this->lastException = $e;
+			// Return fail to caller
+			return false;
+		}
 	}
 
     /**
-     * @param string $key
-     * @param string $value
-     * @param int|\DateInterval $lifetime
+     * @param string  $key
+     * @param string  $value
+     * @param integer $lifetime
+     *
      * @return bool
      */
     public function save($key, $value, $lifetime = 0)
     {
-		return $this->cache->set($key, $value, $lifetime);
+		// Reset last exception
+		$this->lastException = null;
+		
+		try {
+			// Send value to implementing library.
+			$success = $this->cache->set($key, $value, $lifetime);
+
+			// Return boolean from implementing library.
+			return $success;
+
+		// "Exception interface for invalid cache arguments."
+		} catch (\Psr\SimpleCache\InvalidArgumentException $e) {
+			// Save exception for retrieval by caller 
+			$this->lastException = $e;
+			// Return fail to caller
+			return false;
+
+		// "Interface used for all types of exceptions thrown by the implementing library."
+		} catch (\Psr\SimpleCache\CacheException $e) {
+			// Save exception for retrieval by caller 
+			$this->lastException = $e;
+			// Return fail to caller
+			return false;
+		}
     }
 }
