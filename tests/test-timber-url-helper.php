0daa8b78e1b9c8f8172362443bfd7d3e85400ee1
Oskar Schöldström <m@oxy.fi>
2017-08-05 16:20:19 -0500

    add test for new URLHelper::get_root_dir()

--- tests/test-timber-url-helper.php
+++ tests/test-timber-url-helper.php
@@ -68,6 +68,11 @@
             $this->assertEquals('/wp-content', $subdir);
         }
 
+        function testRootDir() {
+            $rootDir = Timber\URLHelper::get_root_dir();
+            $this->assertEquals(ABSPATH, $rootDir);
+        }
+
         function testURLToFileSystem() {
             $url = 'http://example.org/wp-content/uploads/2012/06/mypic.jpg';
             $file = TimberURLHelper::url_to_file_system($url);
