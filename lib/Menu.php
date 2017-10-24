5790e61126fad3e24c58dfa24ef3b14924eab212
Chris Lagasse <chris@bigseadesign.com>
2017-10-24 09:54:40 -0400

    Set it up so TimberMenu can support 'Depth' argument, like wp_nav_menu would.

--- lib/Menu.php
+++ lib/Menu.php
@@ -12,6 +12,12 @@ class Menu extends Core {
 
 	/**
 	 * @api
+	 * @var integer The depth of the menu we are rendering
+	 */
+	public $depth;
+
+	/**
+	 * @api
 	 * @var array|null Array of `Timber\Menu` objects you can to iterate through.
 	 */
 	public $items = null;
@@ -42,9 +48,17 @@ class Menu extends Core {
 
 	/**
 	 * @api
+	 * @var array The unfiltered options sent forward via the user in the __construct
+	 */
+	public $raw_options;
+
+	/**
+	 * @api
 	 * @var string The name of the menu (ex: `Main Navigation`).
 	 */
 	public $title;
+
+
 
 	/**
 	 * Initialize a menu.
@@ -53,9 +67,12 @@ class Menu extends Core {
 	 *                         menu, the slug of theregistered location or nothing. Passing nothing
 	 *                         is good if you only have one menu. Timber will grab what it finds.
 	 */
-	public function __construct( $slug = 0 ) {
+	public function __construct( $slug = 0, $options = array()) {
 		$menu_id = false;
 		$locations = get_nav_menu_locations();
+
+		$this->set_options($options);
+
 		if ( $slug != 0 && is_numeric($slug) ) {
 			$menu_id = $slug;
 		} else if ( is_array($locations) && count($locations) ) {
@@ -83,6 +100,7 @@ class Menu extends Core {
 			_wp_menu_item_classes_by_context($menu);
 			if ( is_array($menu) ) {
 				$menu = self::order_children($menu);
+				$menu = self::strip_to_depth_limit($menu);
 			}
 			$this->items = $menu;
 			$menu_info = wp_get_nav_menu_object($menu_id);
@@ -91,6 +109,20 @@ class Menu extends Core {
 			$this->id = $this->term_id;
 			$this->title = $this->name;
 		}
+	}
+
+	/**
+	 * @internal
+	 * @param mixed $options
+	 */
+	protected function set_options ($options) {
+		if (!is_array($options)) {
+			return;
+		}
+
+		// Set any important options
+		$this->depth = (isset($options['depth']) ? (int)$options['depth'] : -1);
+		$this->raw_options = $options; // for future enhancements?
 	}
 
 	/**
@@ -208,6 +240,27 @@ class Menu extends Core {
 				$menu[] = $item;
 			}
 		}
+		return $menu;
+	}
+
+	/**
+	 * @internal
+	 * @param array $menu
+	 */
+	protected function strip_to_depth_limit ($menu, $current = 1) {
+		if ($this->depth <= 0) {
+			return $menu;
+		}
+
+		foreach ($menu as &$currentItem) {
+			if ($current == $this->depth) {
+				$currentItem->children = false;
+				continue;
+			}
+
+			$currentItem->children = self::strip_to_depth_limit($currentItem->children, $current + 1);
+		}
+
 		return $menu;
 	}
 
