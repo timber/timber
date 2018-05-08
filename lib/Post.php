e34a702c367b5a710a09e153878f532c77805ce1
Nick Braica <nick.braica@gmail.com>
2018-05-08 19:13:07 -0400

    If previewing a post, Timber should make sure to get the preview ID

--- lib/Post.php
+++ lib/Post.php
@@ -251,6 +251,15 @@ class Post extends Core implements CoreInterface {
 		if ( $pid === null && ($pid_from_loop = PostGetter::loop_to_id()) ) {
 			$pid = $pid_from_loop;
 		}
+		if (
+			isset($wp_query->queried_object_id)
+			&& ($wp_query->queried_object_id === $pid || (is_object($pid) && $wp_query->queried_object_id === $pid->ID))
+			&& isset($_GET['preview'])
+			&& isset($_GET['preview_nonce'])
+			&& wp_verify_nonce($_GET['preview_nonce'], 'post_preview_'.$wp_query->queried_object_id)
+		) {
+			$pid = $this->get_post_preview_id($wp_query);
+		}
 		return $pid;
 	}
 
