2c33fabb7bacca9002585456b77238e262e7299c
Jared Novack <jarednova@upstatement.com>
2018-05-14 17:02:38 -0400

    ref #1712 -- added failing test for preview

--- lib/Post.php
+++ lib/Post.php
@@ -217,6 +217,7 @@ class Post extends Core implements CoreInterface {
 	 */
 	protected static function is_previewing() {
 		global $wp_query;
+		error_log('is previewing');
 		if ( isset($_GET['preview']) && isset($_GET['preview_nonce']) && wp_verify_nonce($_GET['preview_nonce'], 'post_preview_'.$wp_query->queried_object_id) ) {
 			return true;
 		}
