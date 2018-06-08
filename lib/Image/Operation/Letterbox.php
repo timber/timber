5fffd8f861e84ec945abee71a783491b5b2c3d7f
Maciej Palmowski <m.palmowski@spiders.agency>
2018-06-08 11:10:01 +0200

    Update Letterbox.php

--- lib/Image/Operation/Letterbox.php
+++ lib/Image/Operation/Letterbox.php
@@ -57,6 +57,11 @@ class Letterbox extends ImageOperation {
 	 * @return bool                  true if everything went fine, false otherwise
 	 */
 	public function run( $load_filename, $save_filename ) {
+		// attempt to check if SVG
+		if ( ImageHelper::is_svg( $load_filename ) ) {
+			return false;
+		}
+		
 		$w = $this->w;
 		$h = $this->h;
 
