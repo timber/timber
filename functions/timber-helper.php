b0d4d32ed2011e9c3be4315b7e8f75ed986ab3af
Kiriakos Diamantis <replicator31@hotmail.com>
2018-12-06 12:40:54 +0200

    Change words translation context to use wordpress core content and functionality

--- functions/timber-helper.php
+++ functions/timber-helper.php
@@ -175,7 +175,7 @@ class TimberHelper {
         $text = strip_tags( $text, $allowed_tag_string );
         /* translators: If your word count is based on single characters (East Asian characters),
         enter 'characters'. Otherwise, enter 'words'. Do not translate into your own language. */
-        if ( 'characters' == _x( 'words', 'word count: words or characters?' ) && preg_match( '/^utf\-?8$/i', get_option( 'blog_charset' ) ) ) {
+        if ( strpos( _x( 'words', 'Word count type. Do not translate!' ), 'characters' ) === 0 && preg_match( '/^utf\-?8$/i', get_option( 'blog_charset' ) ) ) {
             $text = trim( preg_replace( "/[\n\r\t ]+/", ' ', $text ), ' ' );
             preg_match_all( '/./u', $text, $words_array );
             $words_array = array_slice( $words_array[0], 0, $num_words + 1 );
