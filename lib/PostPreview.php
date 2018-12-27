1a341fbee3c28e26cd0d08a2d2ab63d485cb5238
Jared Novack <jarednova@upstatement.com>
2018-12-27 18:20:11 -0500

    Simplify conditionals

--- lib/PostPreview.php
+++ lib/PostPreview.php
@@ -122,10 +122,9 @@ class PostPreview {
 		if ( isset($this->post->post_excerpt) && strlen($this->post->post_excerpt) ) {
 			$text = $this->post->post_excerpt;
 			if ( $this->force ) {
+				$text = TextHelper::trim_words($text, $len, false);
 				if ( $allowable_tags ) {
 					$text = TextHelper::trim_words($text, $len, false, strtr($allowable_tags, '<>', '  '));
-				} else {
-					$text = TextHelper::trim_words($text, $len, false);
 				}
 				if ( $chars !== false ) {
 					$text = TextHelper::trim_characters($text, $chars, false);
@@ -137,10 +136,9 @@ class PostPreview {
 			$pieces = explode($readmore_matches[0], $this->post->post_content);
 			$text = $pieces[0];
 			if ( $force ) {
+				$text = TextHelper::trim_words($text, $len, false);
 				if ( $allowable_tags ) {
 					$text = TextHelper::trim_words($text, $len, false, strtr($allowable_tags, '<>', '  '));
-				} else {
-					$text = TextHelper::trim_words($text, $len, false);
 				}
 				if ( $chars !== false ) {
 					$text = TextHelper::trim_characters($text, $chars, false);
