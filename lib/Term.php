7ea0ddbcce1812e92dc2531b4514e98a7a8d9a24
Lukas Gaechter <lukas.gaechter@mind.ch>
2019-03-03 19:52:43 +0100

    Update variable names

--- lib/Term.php
+++ lib/Term.php
@@ -248,24 +248,30 @@ class Term extends Core implements CoreInterface {
 	}
 
 	/**
-	 * Get Posts that have been "tagged" with the particular term
+	 * Get posts that have the current term assigned.
+	 *
 	 * @internal
-	 * @param int $numberposts
-	 * @param string $post_type
-	 * @param string $PostClass
+	 * @param int|array $numberposts_or_args Optional. Either the number of posts or an array of
+	 *                                       arguments for the post query that this method is going.
+	 *                                       to perform. Default `10`.
+	 * @param string    $post_type_or_class  Optional. Either the post type to get or the name of
+	 *                                       post class to use for the returned posts. Default
+	 *                                       `any`.
+	 * @param string    $post_class          Optional. The name of the post class to use for the
+	 *                                       returned posts. Default `Timber\Post`.
 	 * @return array|bool|null
 	 */
-	public function get_posts( $numberposts = 10, $post_type = 'any', $PostClass = '' ) {
-		if ( !strlen($PostClass) ) {
-			$PostClass = $this->PostClass;
+	public function get_posts( $numberposts_or_args = 10, $post_type_or_class = 'any', $post_class = '' ) {
+		if ( !strlen($post_class) ) {
+			$post_class = $this->PostClass;
 		}
 		$default_tax_query = array(array(
 			'field' => 'term_id',
 			'terms' => $this->ID,
 			'taxonomy' => $this->taxonomy,
 		));
-		if ( is_string($numberposts) && strstr($numberposts, '=') ) {
-			$args = $numberposts;
+		if ( is_string( $numberposts_or_args) && strstr( $numberposts_or_args, '=') ) {
+			$args     = $numberposts_or_args;
 			$new_args = array();
 			parse_str($args, $new_args);
 			$args = $new_args;
@@ -273,29 +279,29 @@ class Term extends Core implements CoreInterface {
 			if ( !isset($args['post_type']) ) {
 				$args['post_type'] = 'any';
 			}
-			if ( class_exists($post_type) ) {
-				$PostClass = $post_type;
+			if ( class_exists($post_type_or_class) ) {
+				$post_class = $post_type_or_class;
 			}
-		} else if ( is_array($numberposts) ) {
+		} else if ( is_array( $numberposts_or_args) ) {
 			//they sent us an array already baked
-			$args = $numberposts;
+			$args = $numberposts_or_args;
 			if ( !isset($args['tax_query']) ) {
 				$args['tax_query'] = $default_tax_query;
 			}
-			if ( class_exists($post_type) ) {
-				$PostClass = $post_type;
+			if ( class_exists($post_type_or_class) ) {
+				$post_class = $post_type_or_class;
 			}
 			if ( !isset($args['post_type']) ) {
 				$args['post_type'] = 'any';
 			}
 		} else {
 			$args = array(
-				'numberposts' => $numberposts,
-				'tax_query' => $default_tax_query,
-				'post_type' => $post_type
+				'numberposts' => $numberposts_or_args,
+				'tax_query'   => $default_tax_query,
+				'post_type'   => $post_type_or_class
 			);
 		}
-		return Timber::get_posts($args, $PostClass);
+		return Timber::get_posts($args, $post_class);
 	}
 
 	/**
@@ -414,7 +420,7 @@ class Term extends Core implements CoreInterface {
 	}
 
 	/**
-	 * Gets posts that have this term assigned.
+	 * Gets posts that have the current term assigned.
 	 *
 	 * @api
 	 * @example
