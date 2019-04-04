d93f56ae96ce40cff24dd71c147d455542f5c49b
Lukas Gaechter <lukas.gaechter@mind.ch>
2019-04-04 18:41:58 +0200

    Remove remaining version_compare()

--- lib/Twig_Function.php
+++ lib/Twig_Function.php
@@ -5,23 +5,23 @@ namespace Timber;
 /**
  * Handle TwigFunction among different Twig versions.
  *
- * From Twig 2.4.0, extending Twig_Function is deprecated and will be final in 3.0.
- * Temporary fix for conflicts between Twig_Function and Twig_SimpleFunction
- * in different versions of Twig (1.* and 2.*).
+ * Temporary fixes for conflicts between Twig_Function and Twig_SimpleFunction in different versions
+ * of Twig (1.*, 2.* and 2.4+). From Twig 2.4.0, extending Twig_Filter is deprecated and the class
+ * will be final in 3.0.
  *
  * @ticket #1641
  */
 
 if ( class_exists( '\Twig\TwigFunction' ) ) {
-
+	// Twig version >= 2.4 with namespaced classes.
 	class_alias('\Twig\TwigFunction', '\Timber\Twig_Function');
 
-} elseif ( version_compare(\Twig_Environment::VERSION, '2.0.0', '>=') ) {
-
+} elseif ( class_exists( '\Twig_Function' ) ) {
+	// Twig version >= 2.0.0
 	class Twig_Function extends \Twig_Function { }
 
 } else {
-
+	// Twig 1.x
 	class Twig_Function extends \Twig_SimpleFunction { }
 
 }
