c712bdd11c669d4a54b77e4ed2ff65d3eeaa6c9e
Alex James Bishop <alex@framecreative.com.au>
2019-04-30 18:57:34 +0930

    Fix sed issues on Mac OS
    Pesky little space removed to make the sed command work

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -102,7 +102,7 @@ install_wp() {
 install_test_suite() {
 	# portable in-place argument for both GNU sed and Mac OSX sed
 	if [[ $(uname -s) == 'Darwin' ]]; then
-		local ioption='-i .bak'
+		local ioption='-i.bak'
 	else
 		local ioption='-i'
 	fi
