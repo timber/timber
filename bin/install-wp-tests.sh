63585852edb8974f833f7969339d58d7f07597b1
Alex James Bishop <alex@framecreative.com.au>
2019-04-30 18:58:40 +0930

    Put downloads in the correct place!
    All our work with the new temp paths almost for naught..
    
    Respects $TMPDIR now for downloads.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -92,8 +92,8 @@ install_wp() {
 		else
 			local ARCHIVE_NAME="wordpress-$WP_VERSION"
 		fi
-		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
-		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  $TMPDIR/wordpress.tar.gz
+		tar --strip-components=1 -zxmf $TMPDIR/wordpress.tar.gz -C $WP_CORE_DIR
 	fi
 
 	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
