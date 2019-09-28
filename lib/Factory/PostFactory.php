99165144e9f2f34b3c435393f34998fc72c13018
Coby Tamayo <ctamayo@sitecrafting.com>
2019-09-27 17:11:37 -0700

    #2093 Basic PostFactory implementation

---
+++ lib/Factory/PostFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Timber\Factory;
+
+use Timber\CoreInterface;
+use Timber\Post;
+
+use WP_Post;
+
+/**
+ * Internal API class for instantiating posts
+ */
+class PostFactory {
+  public function get(int $id) {
+    return $this->build(get_post($id));
+  }
+
+  public function build(WP_Post $post) : CoreInterface {
+    // Get the user-configured Class Map
+    $map = apply_filters( 'timber/post/classmap', [
+      'post'       => Post::class,
+      'page'       => Post::class,
+      // TODO special logic for attachments?
+      'attachment' => Attachment::class,
+    ] );
+
+    // If map is a callable, call it to get the class name
+    if ( is_callable($map) ) {
+      $class = $map($post);
+    } else {
+      $class = $map[$post->post_type] ?? null;
+    }
+
+    // If class is a callable, call it to get the actual class name
+    if (is_callable($class)) {
+      $class = $class($post);
+    }
+
+    // If we don't have a post class by now, fallback on the default class
+    $class = $class
+      ?? apply_filters( 'timber/post/classmap/default', Post::class );
+
+    // TODO make Core constructors protected
+    return new $class($post);
+  }
+}
---
+++ tests/test-post-factory.php
@@ -0,0 +1,70 @@
+<?php
+
+use Timber\Post;
+use Timber\Factory\PostFactory;
+
+class MyPost extends Post {}
+class MyPage extends Post {}
+
+/**
+ * @group factory
+ */
+class TestPostFactory extends Timber_UnitTestCase {
+
+	public function testGet() {
+		$post_id = $this->factory->post->create(['post_type' => 'post']);
+		$page_id = $this->factory->post->create(['post_type' => 'page']);
+
+		$postFactory = new PostFactory();
+		$post				 = $postFactory->get($post_id);
+		$page				 = $postFactory->get($page_id);
+
+		$this->assertInstanceOf(Post::class, $post);
+		$this->assertInstanceOf(Post::class, $post);
+	}
+
+	public function testGetWithOverriddenDefault() {
+		$filter = function () {
+			return MyPost::class;
+		};
+		add_filter( 'timber/post/classmap/default', $filter );
+
+		$post_id   = $this->factory->post->create(['post_type' => 'post']);
+		$page_id   = $this->factory->post->create(['post_type' => 'page']);
+		$custom_id = $this->factory->post->create(['post_type' => 'custom']);
+
+		$postFactory = new PostFactory();
+		$post        = $postFactory->get($post_id);
+		$page        = $postFactory->get($page_id);
+		$custom      = $postFactory->get($custom_id);
+
+		$this->assertInstanceOf(Post::class, $post);
+		$this->assertInstanceOf(Post::class, $page);
+		$this->assertInstanceOf(MyPost::class, $custom);
+
+		remove_filter( 'timber/post/classmap/default', $filter );
+	}
+
+	public function testGetWithCallableMap() {
+		function my_class_map() {
+			return function() { return MyPost::class; };
+		};
+		add_filter( 'timber/post/classmap', 'my_class_map' );
+
+		$post_id   = $this->factory->post->create(['post_type' => 'post']);
+		$page_id   = $this->factory->post->create(['post_type' => 'page']);
+		$custom_id = $this->factory->post->create(['post_type' => 'custom']);
+
+		$postFactory = new PostFactory();
+		$post        = $postFactory->get($post_id);
+		$page        = $postFactory->get($page_id);
+		$custom      = $postFactory->get($custom_id);
+
+		$this->assertInstanceOf(MyPost::class, $post);
+		$this->assertInstanceOf(MyPost::class, $page);
+		$this->assertInstanceOf(MyPost::class, $custom);
+
+		remove_filter( 'timber/post/classmap', 'my_class_map' );
+	}
+
+}
