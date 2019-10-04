6b07da279d4015672163e295e45b4dc2bbcda25b
Coby Tamayo <ctamayo@sitecrafting.com>
2019-10-04 12:39:42 -0700

    #2093 implement UserFactory array/object methods

--- lib/Factory/UserFactory.php
+++ lib/Factory/UserFactory.php
@@ -5,6 +5,7 @@ namespace Timber\Factory;
 use Timber\CoreInterface;
 use Timber\User;
 
+use WP_User_Query;
 use WP_User;
 
 class UserFactory {
@@ -12,15 +13,61 @@ class UserFactory {
 		if (is_int($params)) {
 			return $this->from_id($params);
 		}
+
+		if (is_object($params)) {
+			return $this->from_obj($params);
+		}
+
+		if ($this->is_numeric_array($params)) {
+			// we have a numeric array of objects and/or IDs
+			return array_map([$this, 'from'], $params);
+		}
+
+		if (is_array($params)) {
+			// we have a query array to be passed to WP_User_Query::__construct()
+			return $this->from_wp_user_query(new WP_User_Query($params));
+		}
 	}
 
 	protected function from_id(int $id) {
 		return $this->build(get_user_by('id', $id));
 	}
 
+	protected function from_obj(object $obj) {
+		if ($obj instanceof CoreInterface) {
+			// we already have some kind of Timber Core object
+			return $obj;
+		}
+
+		if ($obj instanceof WP_User) {
+			return $this->build($obj);
+		}
+
+		if ($obj instanceof WP_User_Query) {
+			return array_map([$this, 'build'], $obj->get_results());
+		}
+
+		throw new \InvalidArgumentException(sprintf(
+			'Expected an instance of Timber\CoreInterface or WP_User, got %s',
+			get_class($obj)
+		));
+	}
+
+	// @todo return a UserCollection instance?
+	protected function from_wp_user_query(WP_User_Query $query) : Iterable {
+		return array_map([$this, 'build'], $query->get_results());
+	}
+
 	protected function build(WP_User $user) : CoreInterface {
 		$class = apply_filters( 'timber/user/classmap', User::class, $user );
 
 		return new $class($user);
+	}
+
+	protected function is_numeric_array(array $arr) {
+		foreach (array_keys($arr) as $k) {
+			if ( ! is_int($k) ) return false;
+		}
+		return true;
 	}
 }
--- tests/test-user-factory.php
+++ tests/test-user-factory.php
@@ -10,6 +10,11 @@ class SpecialUser extends User {}
  * @group factory
  */
 class TestUserFactory extends Timber_UnitTestCase {
+	public function tearDown() {
+		global $wpdb;
+		$wpdb->query("TRUNCATE TABLE {$wpdb->users}");
+		parent::tearDown();
+	}
 
 	public function testGetUser() {
 		$id = $this->factory->user->create([
@@ -44,6 +49,173 @@ class TestUserFactory extends Timber_UnitTestCase {
 
 		$this->assertInstanceOf(AdminUser::class, $admin);
 		$this->assertInstanceOf(User::class,      $normie);
+
+		remove_filter( 'timber/user/classmap', $my_class_map );
+	}
+
+	public function testGetUserWithArrayOfIds() {
+		$my_class_map = function(string $class, WP_User $user) {
+			return in_array('administrator', $user->roles)
+				? AdminUser::class
+				: $class;
+		};
+		add_filter( 'timber/user/classmap', $my_class_map, 10, 2 );
+
+		$admin_id = $this->factory->user->create([
+			'user_email' => 'me@example.com',
+			'role'       => 'administrator',
+		]);
+		$normie_id = $this->factory->user->create([
+			'user_email' => 'someone@example.com',
+		]);
+
+		$userFactory = new UserFactory();
+
+		// pass a list of IDs
+		list($admin, $normie) = $userFactory->from([$admin_id, $normie_id]);
+
+		$this->assertInstanceOf(AdminUser::class, $admin);
+		$this->assertInstanceOf(User::class,      $normie);
+
+		remove_filter( 'timber/user/classmap', $my_class_map );
+	}
+
+	public function testGetUserFromWpUserObject() {
+		$my_class_map = function(string $class, WP_User $user) {
+			return in_array('administrator', $user->roles)
+				? AdminUser::class
+				: $class;
+		};
+		add_filter( 'timber/user/classmap', $my_class_map, 10, 2 );
+
+		$admin_id = $this->factory->user->create([
+			'user_email' => 'me@example.com',
+			'role'       => 'administrator',
+		]);
+		$normie_id = $this->factory->user->create([
+			'user_email' => 'someone@example.com',
+		]);
+
+		$admin_wp_user  = get_user_by('id', $admin_id);
+		$normie_wp_user = get_user_by('id', $normie_id);
+
+		$userFactory = new UserFactory();
+		$this->assertInstanceOf(AdminUser::class, $userFactory->from($admin_wp_user));
+		$this->assertInstanceOf(User::class, $userFactory->from($normie_wp_user));
+
+		remove_filter( 'timber/user/classmap', $my_class_map );
+	}
+
+	public function testGetUserWithAssortedArray() {
+		$my_class_map = function(string $class, WP_User $user) {
+			return in_array('administrator', $user->roles)
+				? AdminUser::class
+				: $class;
+		};
+		add_filter( 'timber/user/classmap', $my_class_map, 10, 2 );
+
+		$admin_id = $this->factory->user->create([
+			'user_email' => 'me@example.com',
+			'role'       => 'administrator',
+		]);
+		$normie_id = $this->factory->user->create([
+			'user_email' => 'someone@example.com',
+		]);
+		$editor_id = $this->factory->user->create([
+			'user_login' => 'ddd',
+			'user_email' => 'ed@example.com',
+			'role'       => 'editor',
+		]);
+
+		// create instances of different kinds of user objects
+		$userFactory    = new UserFactory();
+		$admin_user     = $userFactory->from($admin_id);
+		$normie_wp_user = get_user_by('id', $normie_id);
+
+		// pass an array containing a User, a WP_User instance, and an ID
+		$users = $userFactory->from([$admin_user, $normie_wp_user, $editor_id]);
+
+		$this->assertInstanceOf(AdminUser::class, $users[0]);
+		$this->assertInstanceOf(User::class,      $users[1]);
+		$this->assertInstanceOf(User::class,      $users[2]);
+
+		remove_filter( 'timber/user/classmap', $my_class_map );
+	}
+
+	public function testGetUserWithQueryArray() {
+		$my_class_map = function(string $class, WP_User $user) {
+			return in_array('administrator', $user->roles)
+				? AdminUser::class
+				: $class;
+		};
+		add_filter( 'timber/user/classmap', $my_class_map, 10, 2 );
+
+		$subscriber_id = $this->factory->user->create([
+			'user_login' => 'aaa',
+			'user_email' => 'sub@example.com',
+			'role'       => 'subscriber',
+		]);
+		$admin_id = $this->factory->user->create([
+			'user_login' => 'bbb',
+			'user_email' => 'administrator@example.com',
+			'role'       => 'administrator',
+		]);
+		$author_id = $this->factory->user->create([
+			'user_login' => 'ccc',
+			'user_email' => 'author@example.com',
+			'role'       => 'author',
+		]);
+
+		// create instances of different kinds of user objects
+		$userFactory    = new UserFactory();
+
+		// pass an array containing a User and WP_User instance
+		$users = $userFactory->from([
+			'role__in' => ['administrator', 'author'],
+		]);
+
+		$this->assertCount(2, $users);
+		$this->assertInstanceOf(AdminUser::class, $users[0]);
+		$this->assertInstanceOf(User::class,      $users[1]);
+
+		remove_filter( 'timber/user/classmap', $my_class_map );
+	}
+
+	public function testGetUserWithUserQuery() {
+		$my_class_map = function(string $class, WP_User $user) {
+			return in_array('administrator', $user->roles)
+				? AdminUser::class
+				: $class;
+		};
+		add_filter( 'timber/user/classmap', $my_class_map, 10, 2 );
+
+		$subscriber_id = $this->factory->user->create([
+			'user_login' => 'aaa',
+			'user_email' => 'sub@example.com',
+		]);
+		$admin_id = $this->factory->user->create([
+			'user_login' => 'bbb',
+			'user_email' => 'admin@example.com',
+			'role'       => 'administrator',
+		]);
+		$author_id = $this->factory->user->create([
+			'user_login' => 'ccc',
+			'user_email' => 'author@example.com',
+			'role'       => 'author',
+		]);
+
+		// create instances of different kinds of user objects
+		$userFactory    = new UserFactory();
+		$userQuery      = new WP_User_Query([
+			'role__in' => ['administrator', 'author'],
+		]);
+
+		// pass an array containing a User and WP_User instance
+		$users = $userFactory->from($userQuery);
+
+		$this->assertCount(2, $users);
+		$this->assertInstanceOf(AdminUser::class, $users[0]);
+		$this->assertInstanceOf(User::class,      $users[1]);
 
 		remove_filter( 'timber/user/classmap', $my_class_map );
 	}
