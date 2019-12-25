7fdbdcd142f520fe2e512c41f43c5fcf5c0a96ba
Jared Novack <jared@upstatement.com>
2019-12-25 06:51:13 -0500

    Resolve failing test that depended on custom WP stuff w date filter

--- tests/test-timber-twig-filters.php
+++ tests/test-timber-twig-filters.php
@@ -83,8 +83,8 @@
 
 		function testTwigFilterDateWordPressOption(){
 			$format = get_option('date_format');
-			$str = Timber::compile_string("{{now|date('".$format."')}}");
-			$empty = Timber::compile_string("{{now|date}}");
+			$str = Timber::compile_string("{{now|intl_date('".$format."')}}");
+			$empty = Timber::compile_string("{{now|intl_date}}");
 			$this->assertSame($str, $empty);
 		}
 
