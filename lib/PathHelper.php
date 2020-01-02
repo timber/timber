dbdc0ec0603991ccae9a16f921c95a12fc2a8e20
Lukas Gaechter <lukas.gaechter@mind.ch>
2020-01-02 15:54:08 +0100

    Improve inline documentation

--- lib/PathHelper.php
+++ lib/PathHelper.php
@@ -3,20 +3,30 @@
 namespace Timber;
 
 /**
+ * Class PathHelper
+ *
  * Useful methods for working with file paths.
+ *
+ * @api
+ * @since 1.11.1
  */
 class PathHelper {
-
 	/**
+	 * Returns information about a file path.
 	 *
-	 * Unicode-friendly version of the PHP pathinfo() function.
-	 * https://www.php.net/manual/en/function.pathinfo.php
+	 * Unicode-friendly version of PHP’s pathinfo() function.
 	 *
-	 * @param string $path the path.
-	 * @param int    $options the path part to extract.
+	 * @link  https://www.php.net/manual/en/function.pathinfo.php
+	 *
+	 * @api
+	 * @since 1.11.1
+	 *
+	 * @param string $path    The path to be parsed.
+	 * @param int    $options The path part to extract. One of `PATHINFO_DIRNAME`,
+	 *                        `PATHINFO_BASENAME`, `PATHINFO_EXTENSION` or `PATHINFO_FILENAME`. If
+	 *                        not specified, returns all available elements.
+	 *
 	 * @return mixed
-	 *
-	 * @package Timber
 	 */
 	public static function pathinfo( $path, $options = PATHINFO_DIRNAME |
 		PATHINFO_BASENAME | PATHINFO_EXTENSION | PATHINFO_FILENAME
@@ -29,27 +39,38 @@ class PathHelper {
 			),
 			$options
 		);
+
 		if ( is_array( $info ) ) {
-			// decode all keys in the array.
+			// Decode all keys in the array.
 			return array_map( 'rawurldecode', $info );
 		} else {
-			// decode the string when requesting a single path component.
+			// Decode the string when requesting a single path component.
 			return rawurldecode( $info );
 		}
 	}
 
 	/**
+	 * Returns trailing name component of path.
 	 *
 	 * Unicode-friendly version of the PHP basename() function.
-	 * https://www.php.net/manual/en/function.basename.php
 	 *
-	 * @param string $path the path.
-	 * @param string $suffix optional suffix.
+	 * @link  https://www.php.net/manual/en/function.basename.php
+	 *
+	 * @api
+	 * @since 1.11.1
+	 *
+	 * @param string $path   The path.
+	 * @param string $suffix Optional. If the name component ends in suffix, this part will also be
+	 *                       cut off.
+	 *
 	 * @return string
 	 */
 	public static function basename( $path, $suffix = '' ) {
 		return rawurldecode(
-			basename( str_replace( array( '%2F', '%5C' ), '/', rawurlencode( $path ) ), $suffix )
+			basename(
+				str_replace( array( '%2F', '%5C' ), '/', rawurlencode( $path ) ),
+				$suffix
+			)
 		);
 	}
 }
