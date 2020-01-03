80a650ed2333c37e62e734284aea36b4e2b3a5c5
Jared Novack <jared@upstatement.com>
2020-01-03 16:05:32 -0500

    Remove call to dl('gd');

--- tests/test-timber-image-letterbox.php
+++ tests/test-timber-image-letterbox.php
@@ -2,17 +2,10 @@
 
 class TestTimberImageLetterbox extends TimberImage_UnitTestCase {
 
-	static function setUpBeforeClass() {
-		dl('gd');
-		//error_log('gd.so setup via setUpBeforeClass();');
-		
-	}
-
 	function setUp() {
 		parent::setUp();
 		if ( ! extension_loaded( 'gd' ) ) {
 			self::markTestSkipped( 'Letterbox image operation tests requires GD extension' );
-			error_log(print_r(get_loaded_extensions(), true));
 		}
 	}
 
