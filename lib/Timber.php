ca72b63422e1b239b066d4e94468f5eb2cdda0fe
Jared Novack <jared@upstatement.com>
2020-01-04 14:04:11 -0500

    Stub in WP's wp_timezone_string() function which is causing most of the Travis errors in WP 4.9.8

--- lib/Timber.php
+++ lib/Timber.php
@@ -769,4 +769,29 @@ class Timber {
 
 		return Pagination::get_pagination($prefs);
 	}
+
+	/*
+	 * Allow Timber 2.x to be compatible with WP 4.9.8 to WP 5.3. by providing a rip from the 
+	 * WordPress 5.3 source
+	 * 
+	 * @todo remove this once support for versions before WP 5.3 is dropped
+	 */
+	public static function wp_timezone_string() {
+		$timezone_string = get_option( 'timezone_string' );
+	 
+		if ( $timezone_string ) {
+			return $timezone_string;
+		}
+	 
+		$offset  = (float) get_option( 'gmt_offset' );
+		$hours   = (int) $offset;
+		$minutes = ( $offset - $hours );
+	 
+		$sign      = ( $offset < 0 ) ? '-' : '+';
+		$abs_hour  = abs( $hours );
+		$abs_mins  = abs( $minutes * 60 );
+		$tz_offset = sprintf( '%s%02d:%02d', $sign, $abs_hour, $abs_mins );
+	 
+		return $tz_offset;
+}
 }
