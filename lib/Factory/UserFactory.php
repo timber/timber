3f8620b770af2dd5abc849e0df1f62625a47b26d
Coby Tamayo <acobster@gmail.com>
2020-01-08 12:46:08 -0800

    #2094 fix Comment::author()

--- lib/Factory/UserFactory.php
+++ lib/Factory/UserFactory.php
@@ -10,7 +10,7 @@ use WP_User;
 
 class UserFactory {
 	public function from($params) {
-		if (is_int($params)) {
+		if (is_int($params) || is_string($params) && is_numeric($params)) {
 			return $this->from_id($params);
 		}
 
@@ -67,7 +67,10 @@ class UserFactory {
 		return $class::build($user);
 	}
 
-	protected function is_numeric_array(array $arr) {
+	protected function is_numeric_array($arr) {
+		if ( ! is_array($arr) ) {
+			return false;
+		}
 		foreach (array_keys($arr) as $k) {
 			if ( ! is_int($k) ) return false;
 		}
