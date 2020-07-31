2021f8e92f3cf53115b3813377e0f2d5ef715a10
Coby Tamayo <ctamayo@sitecrafting.com>
2020-07-30 18:03:47 -0700

    #2089 test instantiating from nav_menu_item WP_Post instance

--- tests/test-menu-item-factory.php
+++ tests/test-menu-item-factory.php
@@ -31,5 +31,29 @@ class TestMenuItemFactory extends Timber_UnitTestCase {
 		$factory = new MenuItemFactory();
 
 		$this->assertInstanceOf(MenuItem::class, $factory->from($item_id, $menu));
+	}
+
+	public function testMenuFromPost() {
+		// Destructure the result into the menu WP_Term instance
+		// and the *first* item_id
+		[
+			'term' => $menu_term,
+			'item_ids' => [$item_id],
+		] = $this->create_menu_from_posts([
+			[
+				'post_title' => 'Home',
+				'post_status' => 'publish',
+				'post_name' => 'home',
+				'post_type' => 'page',
+				'menu_order' => 1,
+			]
+		]);
+
+		$menu = Timber::get_menu($menu_term['term_id']);
+		$factory = new MenuItemFactory();
+
+		$post = get_post($item_id);
+
+		$this->assertInstanceOf(MenuItem::class, $factory->from($post, $menu));
   }
 }
