31dc888788800df19212a659521543c773dd988c
Jared Novack <jared@upstatement.com>
2022-02-02 09:33:15 -0500

    Update Letterbox.php

--- lib/Image/Operation/Letterbox.php
+++ lib/Image/Operation/Letterbox.php
@@ -116,9 +116,9 @@ class Letterbox extends ImageOperation {
 					$quality = round(10 - $quality);
 				}
 			} else if ( $ext == 'webp' ) {
-                $func = 'imagecreatefromwebp';
-                $save_func = 'imagewebp';
-            }
+				$func = 'imagecreatefromwebp';
+				$save_func = 'imagewebp';
+			}
 			$image = $func($save_filename);
 			imagecopy($bg, $image, $x, $y, 0, 0, $owt, $oht);
 			if ( $save_func === 'imagegif' ) {
