ce60100831ccf51826f3387bff6923b6d2a07f5f
Nicolas Lemoine <nico.lemoine@gmail.com>
2022-07-25 13:32:19 +0200

    Use `yoast/wp-test-utils`
    - Simplifies `tests/bootstrap.php` and its maintenance
    - Makes possible the addition of "pure"/faster unit tests (≠ integration tests) with BrainMonkey
    - Update `Timber_UnitTestCase`
    - Remove `Timber_UnitTestCase::install_translation`, `Timber_UnitTestCase::uninstall_translation` & `Timber_UnitTestCase::restore_locale`
    - Rename `Timber_UnitTestCase::change_locale` to `Timber_UnitTestCase::switch_to_locale`

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,33 +1,24 @@
 <?php
 
-require_once __DIR__ . '/../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+use Yoast\WPTestUtils\WPIntegration;
 
-$_tests_dir = getenv('WP_TESTS_DIR');
+require_once dirname(__DIR__) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
 
-if (!$_tests_dir) {
-    $_tests_dir = rtrim(sys_get_temp_dir(), '/\\') . '/wordpress-tests-lib';
-}
+$_tests_dir = Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir();
 
-// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
-$_phpunit_polyfills_path = getenv('WP_TESTS_PHPUNIT_POLYFILLS_PATH');
-if (false !== $_phpunit_polyfills_path) {
-    define('WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path);
-}
-
-if (!file_exists("{$_tests_dir}/includes/functions.php")) {
-    echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+if (!is_dir($_tests_dir)) {
+    echo "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh <db-name> <db-user> <db-pass> [db-host] [wp-version] [skip-database-creation]?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
     exit(1);
 }
 
-// Give access to tests_add_filter() function.
-require_once "{$_tests_dir}/includes/functions.php";
+// Get access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
 
 /**
- * Manually load the plugin being tested.
+ * Callback to manually load the plugin
  */
 function _manually_load_plugin()
 {
-    require dirname(__FILE__) . '/../vendor/autoload.php';
     \Timber\Timber::init();
 
     require dirname(__FILE__) . '/../wp-content/plugins/advanced-custom-fields/acf.php';
@@ -36,38 +27,14 @@ function _manually_load_plugin()
     }
 }
 
+// Add plugin to active mu-plugins to make sure it gets loaded.
 tests_add_filter('muplugins_loaded', '_manually_load_plugin');
 
-// Start up the WP testing environment.
-require "{$_tests_dir}/includes/bootstrap.php";
-
-require_once __DIR__ . '/Timber_UnitTestCase.php';
-require_once __DIR__ . '/TimberAttachment_UnitTestCase.php';
-require_once __DIR__ . '/timber-mock-classes.php';
-
-if (!function_exists('is_post_type_viewable')) {
-    function is_post_type_viewable($post_type_object)
-    {
-        return $post_type_object->publicly_queryable || ($post_type_object->_builtin && $post_type_object->public);
-    }
-}
-
-/**
- * This constant is always defined by WPML.
- */
+// WPML integration
 define('ICL_LANGUAGE_CODE', 'en');
 
-/**
- * Mocked function for testing menus in WPML
+/*
+ * Bootstrap WordPress. This will also load the Composer autoload file, the PHPUnit Polyfills
+ * and the custom autoloader for the TestCase and the mock object classes.
  */
-function wpml_object_id_filter($element_id, $element_type = 'post', $return_original_if_missing = false, $language_code = null)
-{
-    $locations = get_nav_menu_locations();
-    if (isset($locations['extra-menu'])) {
-        return $locations['extra-menu'];
-    }
-    return $element_id;
-}
-
-// Make sure translations are installed.
-Timber_UnitTestCase::install_translation('de_DE');
+WPIntegration\bootstrap_it();
