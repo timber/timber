fcb00e7ff2e67e6b7b1199d56a21198ae7addafc
Lukas Gaechter <lukas.gaechter@mind.ch>
2022-08-15 21:53:50 +0200

    Make Request not implement CoreInterface

--- src/Request.php
+++ src/Request.php
@@ -5,9 +5,9 @@ namespace Timber;
 /**
  * Class Request
  *
- * Timber\Request exposes $_GET and $_POST to the context
+ * Timber\Request exposes $_GET and $_POST to the context.
  */
-class Request extends Core implements CoreInterface
+class Request
 {
     public $post = [];
 
@@ -15,7 +15,6 @@ class Request extends Core implements CoreInterface
 
     /**
      * Constructs a Timber\Request object
-     * @example
      */
     public function __construct()
     {
@@ -29,24 +28,5 @@ class Request extends Core implements CoreInterface
     {
         $this->post = $_POST;
         $this->get = $_GET;
-    }
-
-    public function __call($field, $args)
-    {
-    }
-
-    public function __get($field)
-    {
-    }
-
-    /**
-     * @return boolean|null
-     */
-    public function __isset($field)
-    {
-    }
-
-    public function meta($key)
-    {
     }
 }
