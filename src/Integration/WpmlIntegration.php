58c2a2cd843b5536035bcb58bb517e0530af2c9c
Chauncey McAskill <chauncey@mcaskill.ca>
2023-03-03 10:07:05 -0500

    Remove initially proposed filter "timber/menuitem/pre_from"

--- src/Integration/WpmlIntegration.php
+++ src/Integration/WpmlIntegration.php
@@ -19,7 +19,6 @@ class WpmlIntegration implements IntegrationInterface
         add_filter('timber/url_helper/url_to_file_system/path', [$this, 'file_system_to_url'], 10, 1);
         add_filter('timber/menu/id_from_location', [$this, 'menu_object_id_filter'], 10, 1);
         add_filter('timber/menu/items', [$this, 'menu_items_filter'], 10, 1);
-        add_filter('timber/menuitem/pre_from', [$this, 'menu_item_pre_from_filter'], 10, 1);
         add_filter('timber/image_helper/_get_file_url/home_url', [$this, 'file_system_to_url'], 10, 1);
     }
 
