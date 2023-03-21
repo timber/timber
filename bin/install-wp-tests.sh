f89278e9dc22dc728e43cbcece24f6c3f507f18e
Chauncey McAskill <chauncey@mcaskill.ca>
2023-03-21 19:52:02 -0400

    Ensure `TMPDIR` is canonical in tests
    If any symbolic links in `TMPDIR` are not resolved, usage of `realpath()` risks paths being broken such as by `ImageHelper::analyze_url()` calling `URLHelper::remove_url_component()` to remove `WP_CONTENT_DIR`.
    
    Given a WordPress installation at:
    
    ```sh
    /var/folders/XX/XXXXXXXXXX/X/wordpress/
    ```
    
    And the following URL:
    
    ```sh
    http://example.org/wp-content/themes/timber-test-theme/assets/images/cardinals.jpg
    ```
    
    The `ImageHelper::theme_url_to_dir()` method would return the following absolute path (which internally calls `realpath()`):
    
    ```sh
    /private/var/folders/XX/XXXXXXXXXX/X/wordpress/wp-content/themes/timber-test-theme/assets/images/cardinals.jpg
    ```
    
    The `ImageHelper::analyze_url()` method would return the following components:
    
    ```php
    [
      'url'       => 'http://example.org/wp-content/themes/timber-test-theme/assets/images/cardinals.jpg',
      'absolute'  => true,
      'base'      => 2,
      'subdir'    => '/private/themes/timber-test-theme/assets/images',
      'filename'  => 'cardinals',
      'extension' => 'jpg',
      'basename'  => 'cardinals.jpg',
    ]
    ```
    
    As illustrated by `subdir`, the absolute path is broken by `URLHelper::remove_url_component()` striping the symbolically linked path from the canonical path.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -14,6 +14,7 @@ SKIP_DB_CREATE=${6-false}
 
 TMPDIR=${TMPDIR-/tmp}
 TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
+TMPDIR=$(realpath $TMPDIR)
 WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress}
 
