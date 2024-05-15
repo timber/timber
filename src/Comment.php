timber/timber

commit 05af54f7f5463c737299fb9b0512f79b334d2e94

Chore: Update all links in the codebase and documentation to https (#2947)
* chore: update all http:// instances in codebase to https://
* docs: search/replace from http:// to https://

src/Comment.php
@@ -226,7 +226,7 @@ class Comment extends CoreEntity
      * <img src="{{comment.avatar(36,template_uri~"/img/dude.jpg")}}" alt="Image of {{comment.author.name}}" />
      * ```
      * ```html
-     * <img src="http://gravatar.com/i/sfsfsdfasdfsfa.jpg" alt="Image of Katherine Rich" />
+     * <img src="https://gravatar.com/i/sfsfsdfasdfsfa.jpg" alt="Image of Katherine Rich" />
      * ```
      * @param int|mixed    $size     Size of avatar.
      * @param string       $default  Default avatar URL.
@@ -558,9 +558,9 @@ class Comment extends CoreEntity
             $host = 'https://secure.gravatar.com';
         } else {
             if (!empty($email_hash)) {
-                $host = \sprintf("http://%d.gravatar.com", (\hexdec($email_hash[0]) % 2));
+                $host = \sprintf("https://%d.gravatar.com", (\hexdec($email_hash[0]) % 2));
             } else {
-                $host = 'http://0.gravatar.com';
+                $host = 'https://0.gravatar.com';
             }
         }
         return $host;
@@ -597,7 +597,7 @@ class Comment extends CoreEntity
             $default = '';
         } elseif ('gravatar_default' == $default) {
             $default = $host . '/avatar/?s=' . $size;
-        } elseif (empty($email) && !\strstr($default, 'http://')) {
+        } elseif (empty($email) && !\strstr($default, 'https://')) {
             $default = $host . '/avatar/?d=' . $default . '&amp;s=' . $size;
         }
         return $default;

.editorconfig
@@ -2,7 +2,7 @@
 # editorconfig.org
 
 # WordPress Coding Standards
-# http://make.wordpress.org/core/handbook/coding-standards/
+# https://make.wordpress.org/core/handbook/coding-standards/
 
 root = true
 

.github/ISSUE_TEMPLATE/bug_report.yml
@@ -17,7 +17,7 @@ body:
 
       Thanks for taking the time to fill out the issue form. We can help best if you provide as much information as possible.
 
-      Is this a support or usage question? Please post it to [Discussions](https://github.com/timber/timber/discussions) or on Stack Overflow (http://stackoverflow.com/questions/tagged/timber) using the *Timber* tag.
+      Is this a support or usage question? Please post it to [Discussions](https://github.com/timber/timber/discussions) or on Stack Overflow (https://stackoverflow.com/questions/tagged/timber) using the *Timber* tag.
 - type: textarea
   attributes:
     label: Expected Behavior

.github/ISSUE_TEMPLATE/config.yml
@@ -1,4 +1,4 @@
 contact_links:
   - name: Support or usage question for Timber
-    url: http://stackoverflow.com/questions/tagged/timber
+    url: https://stackoverflow.com/questions/tagged/timber
     about: Is this a support or usage question? Please post to Stack Overflow using the Timber tag.

phpunit.xml
@@ -1,4 +1,3 @@
-<?xml version="1.0"?>
 <phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://schema.phpunit.de/9.3/phpunit.xsd" bootstrap="./tests/bootstrap.php" colors="true" convertErrorsToExceptions="true" convertNoticesToExceptions="true" convertWarningsToExceptions="true" backupGlobals="false" stopOnFailure="false">
   <coverage processUncoveredFiles="true">
     <include>

src/Archives.php
@@ -7,7 +7,7 @@ namespace Timber;
  *
  * The `Timber\Archives` class is used to generate a menu based on the date archives of your posts.
  *
- * The [Nieman Foundation News site](http://nieman.harvard.edu/news/) has an example of how the
+ * The [Nieman Foundation News site](https://nieman.harvard.edu/news/) has an example of how the
  * output can be used in a real site ([screenshot](https://cloud.githubusercontent.com/assets/1298086/9610076/3cdca596-50a5-11e5-82fd-acb74c09c482.png)).
  *
  * @api

src/Attachment.php
@@ -92,8 +92,8 @@ class Attachment extends Post
      * <a href="{{ image.link }}"><img src="{{ image.src }} "></a>
      * ```
      * ```html
-     * <a href="http://example.org/my-cool-picture">
-     *     <img src="http://example.org/wp-content/uploads/2015/whatever.jpg"/>
+     * <a href="https://example.org/my-cool-picture">
+     *     <img src="https://example.org/wp-content/uploads/2015/whatever.jpg"/>
      * </a>
      * ```
      *
@@ -166,7 +166,7 @@ class Attachment extends Post
      * <a href="{{ get_attachment(post.meta('job_pdf')).src }}" download>
      * ```
      * ```html
-     * <a href="http://example.org/wp-content/uploads/2015/08/job-ad-5noe2304i.pdf" download>
+     * <a href="https://example.org/wp-content/uploads/2015/08/job-ad-5noe2304i.pdf" download>
      * ```
      *
      * @return string

src/ExternalImage.php
@@ -34,7 +34,7 @@ namespace Timber;
  *
  * ```html
  * <article>
- *   <img src="http://example.org/wp-content/uploads/2015/06/nevermind.jpg" class="cover-image" />
+ *   <img src="https://example.org/wp-content/uploads/2015/06/nevermind.jpg" class="cover-image" />
  *   <h1 class="headline">Now you've done it!</h1>
  *   <div class="body">
  *     Whatever whatever
@@ -185,8 +185,8 @@ class ExternalImage implements ImageInterface
      * <img src="{{ post.thumbnail.src('medium') }}">
      * ```
      * ```html
-     * <img src="http://example.org/wp-content/uploads/2015/08/pic.jpg" />
-     * <img src="http://example.org/wp-content/uploads/2015/08/pic-800-600.jpg">
+     * <img src="https://example.org/wp-content/uploads/2015/08/pic.jpg" />
+     * <img src="https://example.org/wp-content/uploads/2015/08/pic-800-600.jpg">
      * ```
      *
      * @param string $size Ignored. For compatibility with Timber\Image.
@@ -319,7 +319,7 @@ class ExternalImage implements ImageInterface
      * <img src="{{ image.src }}" width="{{ image.width }}" />
      * ```
      * ```html
-     * <img src="http://example.org/wp-content/uploads/2015/08/pic.jpg" width="1600" />
+     * <img src="https://example.org/wp-content/uploads/2015/08/pic.jpg" width="1600" />
      * ```
      *
      * @return int|null The width of the image in pixels. Null if the width can’t be read, e.g. because the file doesn’t
@@ -339,7 +339,7 @@ class ExternalImage implements ImageInterface
      * <img src="{{ image.src }}" height="{{ image.height }}" />
      * ```
      * ```html
-     * <img src="http://example.org/wp-content/uploads/2015/08/pic.jpg" height="900" />
+     * <img src="https://example.org/wp-content/uploads/2015/08/pic.jpg" height="900" />
      * ```
      *
      * @return int|null The height of the image in pixels. Null if the height can’t be read, e.g. because the file
@@ -459,7 +459,7 @@ class ExternalImage implements ImageInterface
      * ```
      * ```html
      * <img
-     *     src="http://example.org/wp-content/uploads/2015/08/pic.jpg"
+     *     src="https://example.org/wp-content/uploads/2015/08/pic.jpg"
      *     alt="You should always add alt texts to your images for better accessibility"
      * />
      * ```

commit af5600359b38fcd2b3a753b4c6f319686fa0299d

fix: Fix a bug with URL check for avatars
Fixes a bug introduced in #2947

src/Comment.php
@@ -588,6 +588,7 @@ class Comment extends CoreEntity
                 $default = $avatar_default;
             }
         }
+
         if ('mystery' == $default) {
             $default = $host . '/avatar/ad516503a11cd5ca435acc9bb6523536?s=' . $size;
             // ad516503a11cd5ca435acc9bb6523536 == md5('unknown@gravatar.com')
@@ -597,7 +598,7 @@ class Comment extends CoreEntity
             $default = '';
         } elseif ('gravatar_default' == $default) {
             $default = $host . '/avatar/?s=' . $size;
-        } elseif (empty($email) && !\strstr($default, 'https://')) {
+        } elseif (empty($email) && !\preg_match('/^https?:\/\//', $default)) {
             $default = $host . '/avatar/?d=' . $default . '&amp;s=' . $size;
         }
         return $default;