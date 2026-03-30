bb39174ee4ffa9c313b9312a4408edd76a863aa1
menno-ll <menno@level-level.com>
2026-03-30 11:55:06 +0200

    use int

--- tests/TimberMultisiteTest.php
+++ tests/TimberMultisiteTest.php
@@ -245,10 +245,9 @@ class TimberMultisiteTest extends TimberIntegrationTestCase
 
         $sites = Timber::get_sites();
         foreach ($sites as $site) {
-            \switch_to_blog($site->blog_id);
+            \switch_to_blog((int) $site->blog_id);
             $ts = new Site();
-            $this->assertSame($site->blog_id, $ts->ID);
-            $this->assertSame($site->blog_id, \get_current_blog_id());
+            $this->assertSame((int) $site->blog_id, \get_current_blog_id());
             \restore_current_blog();
         }
     }
